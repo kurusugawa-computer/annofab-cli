353634d512a8f1bdc9580a31075d6d22523bf664
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-11-15 14:34:21 +0900

    input_data_idを指定できるようにする fix #109

--- annofabcli/input_data/list_input_data.py
+++ annofabcli/input_data/list_input_data.py
@@ -82,53 +82,97 @@ class ListInputData(AbstractCommandLineInterface):
                 task_id_list.append(task['task_id'])
         return task_id_list
 
-    def get_input_data(self, project_id: str, input_data_query: Optional[Dict[str, Any]] = None,
-                       add_details: bool = False) -> List[InputData]:
+    @annofabcli.utils.allow_404_error
+    def get_input_data(self, project_id: str, input_data_id: str) -> InputData:
+        input_data, _ = self.service.api.get_input_data(project_id, input_data_id)
+        return input_data
+
+    def get_input_data_from_input_data_id(self, project_id: str, input_data_id_list: List[str]) -> List[InputData]:
+        input_data_list = []
+        logger.debug(f"{len(input_data_id_list)}件の入力データを取得します。")
+        for index, input_data_id in enumerate(input_data_id_list):
+            if (index + 1) % 100 == 0:
+                logger.debug(f"{index+1} 件目の入力データを取得します。")
+
+            input_data = self.get_input_data(project_id, input_data_id)
+            if input_data is not None:
+                input_data_list.append(input_data)
+            else:
+                logger.warning(f"入力データ '{input_data_id}' は見つかりませんでした。")
+
+        return input_data_list
+
+    def add_details_to_input_data_list(self, project_id: str, input_data_list: List[InputData]) -> List[InputData]:
+        """
+        `input_data_list`に詳細情報（どのタスクに使われているか）を付与する。
+
+        Args:
+            project_id:
+            input_data_list: 入力データList(In/Out)
+
+        Returns:
+
+        """
+        if len(input_data_list) == 0:
+            return input_data_list
+
+        logger.debug(f"入力データ一覧の件数: {len(input_data_list)}")
+
+        # AWS CloudFrontのURLの上限が8,192byte
+        # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/cloudfront-limits.html
+        MAX_URL_QUERY_LENGTH = 8000  # input_dat_ids部分のURLクエリの最大値
+        average_input_data_id_length = self.average_input_data_id_length + 1  # カンマの分だけ長さを増やす
+        chunk_size = MAX_URL_QUERY_LENGTH // average_input_data_id_length
+        initial_index = 0
+        while True:
+            sub_input_data_list = input_data_list[initial_index:initial_index + chunk_size]
+            sub_input_data_id_list = [e['input_data_id'] for e in sub_input_data_list]
+            str_input_data_id_list = ",".join(sub_input_data_id_list)
+            encoded_input_data_id_list = urllib.parse.quote(str_input_data_id_list)
+            if len(encoded_input_data_id_list) > MAX_URL_QUERY_LENGTH:
+                differential_length = (len(encoded_input_data_id_list) - MAX_URL_QUERY_LENGTH)
+                decreasing_size = (differential_length // average_input_data_id_length) + 1
+                logger.debug(f"chunk_sizeを {chunk_size} から、{chunk_size - decreasing_size} に減らした. "
+                             f"len(encoded_input_data_id_list) = {len(encoded_input_data_id_list)}")
+                chunk_size = chunk_size - decreasing_size
+                if chunk_size <= 0:
+                    chunk_size = 1
+
+                continue
+
+            logger.debug(f"input_data_list[{initial_index}:{initial_index+chunk_size}] を使用しているタスクを取得する。")
+            task_list = self.service.wrapper.get_all_tasks(project_id,
+                                                           query_params={'input_data_ids': str_input_data_id_list})
+
+            for input_data in sub_input_data_list:
+                # input_data_idで絞り込んでいるが、大文字小文字を区別しない。
+                # したがって、確認のため `_find_task_id_list`を実行する
+                task_id_list = self._find_task_id_list(task_list, input_data['input_data_id'])
+                self.visualize.add_properties_to_input_data(input_data, task_id_list)
+
+            initial_index = initial_index + chunk_size
+            if initial_index >= len(input_data_list):
+                break
+
+        return input_data_list
+
+    def get_input_data_list(self, project_id: str, input_data_id_list: Optional[List[str]] = None,
+                            input_data_query: Optional[Dict[str, Any]] = None,
+                            add_details: bool = False) -> List[InputData]:
         """
         入力データ一覧を取得する。
         """
+        if input_data_id_list is not None:
+            input_data_list = self.get_input_data_from_input_data_id(project_id, input_data_id_list)
+        else:
+            logger.debug(f"input_data_query: {input_data_query}")
+            input_data_list = self.service.wrapper.get_all_input_data_list(project_id, query_params=input_data_query)
 
-        logger.debug(f"input_data_query: {input_data_query}")
-        input_data_list = self.service.wrapper.get_all_input_data_list(project_id, query_params=input_data_query)
+        logger.debug(f"入力データ一覧の件数: {len(input_data_list)}")
+
         # 詳細な情報を追加する
-        if add_details and len(input_data_list) > 0:
-            logger.debug(f"入力データ一覧の件数: {len(input_data_list)}")
-
-            # AWS CloudFrontのURLの上限が8,192byte
-            # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/cloudfront-limits.html
-            MAX_URL_QUERY_LENGTH = 8000  # input_dat_ids部分のURLクエリの最大値
-            average_input_data_id_length = self.average_input_data_id_length + 1  # カンマの分だけ長さを増やす
-            chunk_size = MAX_URL_QUERY_LENGTH // average_input_data_id_length
-            initial_index = 0
-            while True:
-                sub_input_data_list = input_data_list[initial_index:initial_index + chunk_size]
-                sub_input_data_id_list = [e['input_data_id'] for e in sub_input_data_list]
-                str_input_data_id_list = ",".join(sub_input_data_id_list)
-                encoded_input_data_id_list = urllib.parse.quote(str_input_data_id_list)
-                if len(encoded_input_data_id_list) > MAX_URL_QUERY_LENGTH:
-                    differential_length = (len(encoded_input_data_id_list) - MAX_URL_QUERY_LENGTH)
-                    decreasing_size = (differential_length // average_input_data_id_length) + 1
-                    logger.debug(f"chunk_sizeを {chunk_size} から、{chunk_size - decreasing_size} に減らした. "
-                                 f"len(encoded_input_data_id_list) = {len(encoded_input_data_id_list)}")
-                    chunk_size = chunk_size - decreasing_size
-                    if chunk_size <= 0:
-                        chunk_size = 1
-
-                    continue
-
-                logger.debug(f"input_data_list[{initial_index}:{initial_index+chunk_size}] を使用しているタスクを取得する。")
-                task_list = self.service.wrapper.get_all_tasks(project_id,
-                                                               query_params={'input_data_ids': str_input_data_id_list})
-
-                for input_data in sub_input_data_list:
-                    # input_data_idで絞り込んでいるが、大文字小文字を区別しない。
-                    # したがって、確認のため `_find_task_id_list`を実行する
-                    task_id_list = self._find_task_id_list(task_list, input_data['input_data_id'])
-                    self.visualize.add_properties_to_input_data(input_data, task_id_list)
-
-                initial_index = initial_index + chunk_size
-                if initial_index >= len(input_data_list):
-                    break
+        if add_details:
+            self.add_details_to_input_data_list(project_id, input_data_list)
 
         return input_data_list
 
@@ -162,7 +206,7 @@ class ListInputData(AbstractCommandLineInterface):
                 idq["to"] = to_iso8601_extension(to_datetime)
 
             logger.debug(f"入力データを取得します。query={idq}")
-            input_data_list = self.get_input_data(project_id, idq, add_details)
+            input_data_list = self.get_input_data_list(project_id, input_data_query=idq, add_details=add_details)
             logger.debug(f"入力データを {len(input_data_list)} 件取得しました。")
             if len(input_data_list) == 10000:
                 logger.warning("入力データ一覧は10,000件で打ち切られている可能性があります。")
@@ -172,13 +216,15 @@ class ListInputData(AbstractCommandLineInterface):
         return all_input_data_list
 
     def print_input_data(self, project_id: str, input_data_query: Optional[Dict[str, Any]] = None,
-                         add_details: bool = False, batch_query: Optional[InputDataBatchQuery] = None):
+                         input_data_id_list: Optional[List[str]] = None, add_details: bool = False,
+                         batch_query: Optional[InputDataBatchQuery] = None):
         """
         入力データ一覧を出力する
 
         Args:
             project_id: 対象のproject_id
             input_data_query: 入力データの検索クエリ
+            input_data_id: 入力データID
             add_details: 詳細情報を表示する
 
         """
@@ -186,8 +232,8 @@ class ListInputData(AbstractCommandLineInterface):
         super().validate_project(project_id, project_member_roles=None)
 
         if batch_query is None:
-            input_data_list = self.get_input_data(project_id, input_data_query=input_data_query,
-                                                  add_details=add_details)
+            input_data_list = self.get_input_data_list(project_id, input_data_query=input_data_query,
+                                                       input_data_id_list=input_data_id_list, add_details=add_details)
             logger.info(f"入力データ一覧の件数: {len(input_data_list)}")
             if len(input_data_list) == 10000:
                 logger.warning("入力データ一覧は10,000件で打ち切られている可能性があります。")
@@ -223,12 +269,17 @@ def parse_args(parser: argparse.ArgumentParser):
 
     argument_parser.add_project_id()
 
-    # タスク検索クエリ
-    parser.add_argument(
+    query_group = parser.add_mutually_exclusive_group()
+
+    query_group.add_argument(
         '-iq', '--input_data_query', type=str, help='入力データの検索クエリをJSON形式で指定します。'
         '`file://`を先頭に付けると、JSON形式のファイルを指定できます。'
         'クエリのフォーマットは、[getInputDataList API](https://annofab.com/docs/api/#operation/getInputDataList)のクエリパラメータと同じです。'
         'ただし `page`, `limit`キーは指定できません。')
+
+    query_group.add_argument(
+        '-i', '--input_data_id', type=str, nargs='+', help='対象のinput_data_idを指定します。`--input_data_query`引数とは同時に指定できません。'
+        '`file://`を先頭に付けると、input_data_idの一覧が記載されたファイルを指定できます。')
 
     parser.add_argument(
         '--batch', type=str, help='段階的に入力データを取得するための情報をJSON形式で指定します。 '
