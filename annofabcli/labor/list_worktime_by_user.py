3017382468ba8b54b47d2b3412ab9e66bc444f84
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-11-18 15:39:27 +0900

    微修正

--- annofabcli/labor/list_worktime_by_user.py
+++ annofabcli/labor/list_worktime_by_user.py
@@ -29,7 +29,7 @@ def str_to_datetime(d: str) -> datetime.datetime:
 @dataclass(frozen=True)
 class LaborWorktime:
     """
-    出力用の作業時間情報
+    労務管理情報
     """
     date: str
     organization_id: str
@@ -42,6 +42,17 @@ class LaborWorktime:
     worktime_plan_hour: float
     worktime_result_hour: float
 
+@dataclass_json
+@dataclass(frozen=True)
+class SumLaborWorktime:
+    """
+    出力用の作業時間情報
+    """
+    date: str
+    user_id: str
+    worktime_plan_hour: float
+    worktime_result_hour: float
+
 
 class ListWorktimeByUser(AbstractCommandLineInterface):
     DATE_FORMAT = "%Y-%m-%d"
@@ -49,31 +60,6 @@ class ListWorktimeByUser(AbstractCommandLineInterface):
     """
     作業時間をユーザごとに出力する。
     """
-
-    @annofabcli.utils.allow_404_error
-    def get_supplementary_data_list(self, project_id: str, input_data_id: str) -> SupplementaryData:
-        supplementary_data_list, _ = self.service.api.get_supplementary_data_list(project_id, input_data_id)
-        return supplementary_data_list
-
-    def get_all_supplementary_data_list(self, project_id: str,
-                                        input_data_id_list: List[str]) -> List[SupplementaryData]:
-        """
-        補助情報一覧を取得する。
-        """
-        all_supplementary_data_list: List[SupplementaryData] = []
-
-        logger.debug(f"{len(input_data_id_list)}件の入力データに紐づく補助情報を取得します。")
-        for index, input_data_id in enumerate(input_data_id_list):
-            if (index + 1) % 100 == 0:
-                logger.debug(f"{index + 1} 件目の入力データを取得します。")
-
-            supplementary_data_list = self.get_supplementary_data_list(project_id, input_data_id)
-            if supplementary_data_list is not None:
-                all_supplementary_data_list.extend(supplementary_data_list)
-            else:
-                logger.warning(f"入力データ '{input_data_id}' に紐づく補助情報が見つかりませんでした。")
-
-        return all_supplementary_data_list
 
     @staticmethod
     def get_member_from_user_id(organization_member_list: List[OrganizationMember], user_id: str) -> Optional[
@@ -110,39 +96,37 @@ class ListWorktimeByUser(AbstractCommandLineInterface):
         organization_member_list = self.service.wrapper.get_all_organization_members(organization_name)
         project_list = self.service.wrapper.get_all_projects_of_organization(organization_name)
 
-        labor_list = \
-            self.service.api.get_labor_control(
-                {"organization_id": organization_id, "from": start_date, "to": end_date})[0][
-                "list"]
+        labor_list, _ = self.service.api.get_labor_control(
+                {"organization_id": organization_id, "from": start_date, "to": end_date})
+        logger.info(f"'{organization_name}'組織の労務管理情報の件数: {len(labor_list)}")
         for labor in labor_list:
             member = self.get_member_from_account_id(organization_member_list, labor["account_id"])
-
             new_labor = LaborWorktime(
                 date=labor["date"],
                 organization_id=labor["organization_id"],
-                organization_name=labor["organization_name"],
+                organization_name=organization_name,
                 project_id=labor["project_id"],
                 project_title=self.get_project_title(project_list, labor["project_id"]),
                 account_id=labor["account_id"],
                 user_id=member["user_id"] if member is not None else "",
                 username=member["username"] if member is not None else "",
-                worktime_plan_hour=self.get_worktime_hour(labor["working_time_by_user"], "plans"),
-                worktime_result_hour=self.get_worktime_hour(labor["working_time_by_user"], "results"),
+                worktime_plan_hour=self.get_worktime_hour(labor["values"]["working_time_by_user"], "plans"),
+                worktime_result_hour=self.get_worktime_hour(labor["values"]["working_time_by_user"], "results"),
             )
             new_labor_list.append(new_labor)
 
         return new_labor_list
 
     @staticmethod
-    def get_sum_worktime_list(labor_list: List[LaborWorktime], user_id: str, start_date: str, end_date: str):
+    def get_sum_worktime_list(labor_list: List[LaborWorktime], user_id: str, start_date: str, end_date: str) -> List[SumLaborWorktime]:
         sum_labor_list = []
         for date in pandas.date_range(start=start_date, end=end_date):
             str_date = date.strftime(ListWorktimeByUser.DATE_FORMAT)
             filtered_list = [e for e in labor_list if e.user_id == user_id and e.date == str_date]
-            worktime_plan_hour = sum([e["worktime_plan_hour"] for e in filtered_list])
-            worktime_result_hour = sum([e["worktime_result_hour"] for e in filtered_list])
+            worktime_plan_hour = sum([e.worktime_plan_hour for e in filtered_list])
+            worktime_result_hour = sum([e.worktime_result_hour for e in filtered_list])
 
-            labor = LaborWorktime(user_id=user_id, date=date, worktime_plan_hour=worktime_plan_hour, worktime_result_hour=worktime_result_hour)
+            labor = SumLaborWorktime(user_id=user_id, date=date, worktime_plan_hour=worktime_plan_hour, worktime_result_hour=worktime_result_hour)
             sum_labor_list.append(labor)
 
         return sum_labor_list
@@ -194,8 +178,7 @@ def parse_args(parser: argparse.ArgumentParser):
     parser.add_argument("--start_date", type=str, required=True, help="集計期間の開始日(%%Y-%%m-%%d)")
     parser.add_argument("--end_date", type=str, required=True, help="集計期間の終了日(%%Y-%%m-%%d)")
 
-    argument_parser.add_output()
-    argument_parser.add_csv_format()
+    parser.add_argument('-o', '--output_dir', type=str, required=True, help='出力先のディレクトリのパス')
 
     parser.set_defaults(subcommand_func=main)
 
