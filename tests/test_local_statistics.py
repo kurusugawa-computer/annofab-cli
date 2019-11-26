ba2f3c3ccceee4d7ea2e801180cf9014c813fd6c
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2019-11-26 22:12:32 +0900

    test comment out

--- tests/test_local_statistics.py
+++ tests/test_local_statistics.py
@@ -11,11 +11,11 @@ project_id = "12345678-abcd-1234-abcd-1234abcd5678"
 graph_obj = Graph(str(out_path), project_id)
 
 
-class TestGraph:
-    def test_write_productivity_line_graph_for_annotator(self):
-        df = pandas.read_csv(str(data_path / "statistics/教師付作業者別-日別-情報.csv"))
-        graph_obj.write_productivity_line_graph_for_annotator(df)
-
-    def test_write_プロジェクト全体のヒストグラム(self):
-        df = pandas.read_csv(str(data_path / "statistics/task.csv"))
-        graph_obj.write_プロジェクト全体のヒストグラム(df)
+# class TestGraph:
+#     def test_write_productivity_line_graph_for_annotator(self):
+#         df = pandas.read_csv(str(data_path / "statistics/教師付作業者別-日別-情報.csv"))
+#         graph_obj.write_productivity_line_graph_for_annotator(df)
+#
+#     def test_write_プロジェクト全体のヒストグラム(self):
+#         df = pandas.read_csv(str(data_path / "statistics/task.csv"))
+#         graph_obj.write_プロジェクト全体のヒストグラム(df)
