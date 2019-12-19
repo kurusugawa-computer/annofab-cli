1209c17d8d68c886399ee5a3f1577a2c20cdb88f
kondoumizuki <mizuki.kondou3@gmail.com>
2019-12-19 13:35:31 +0900

    user_idの部分一致で出力できるようにした

--- annofabcli/experimental/list_labor_worktime.py
+++ annofabcli/experimental/list_labor_worktime.py
@@ -42,9 +42,10 @@ class Database():
 
 
 class FutureTable(Table):
-    def __init__(self, database: Database, task_query_param=None):
+    def __init__(self, database: Database, task_query_param=None, user_id_list: List[str] = None):
         super().__init__(database, task_query_param)
         self.database = database
+        self.user_id_search_list = [user_id.upper() for user_id in user_id_list]
 
     def create_labor_control_df(self):
 
@@ -64,14 +65,28 @@ class FutureTable(Table):
                 else:
                     aw_results = int(l["values"]["working_time_by_user"]["results"]) / 60000
 
-            labor_control_list.append({
-                "account_id": l["account_id"],
-                "date": l["date"],
-                "aw_plans": aw_plans,
-                "aw_results": aw_results,
-                "username": self._get_username(l["account_id"]),
-                "af_time": 0.0
-            })
+            if l["account_id"] == None:
+                continue
+            if self.user_id_search_list == None:
+                labor_control_list.append({
+                    "account_id": l["account_id"],
+                    "date": l["date"],
+                    "aw_plans": aw_plans,
+                    "aw_results": aw_results,
+                    "username": self._get_username(l["account_id"]),
+                    "af_time": 0.0
+                })
+            else:
+                user_id_bool_list = [user_id_search in self._get_user_id(l["account_id"]).upper() for user_id_search in self.user_id_search_list]
+                if True in user_id_bool_list:
+                    labor_control_list.append({
+                        "account_id": l["account_id"],
+                        "date": l["date"],
+                        "aw_plans": aw_plans,
+                        "aw_results": aw_results,
+                        "username": self._get_username(l["account_id"]),
+                        "af_time": 0.0
+                    })
 
         return labor_control_list
 
@@ -85,16 +100,29 @@ class FutureTable(Table):
 
             account_id = account_info['account_id']
             histories = account_info['histories']
+            if account_id == None:
+                continue
+            elif self.user_id_search_list == None:
+                for history in histories:
+                    history['af_time'] = annofabcli.utils.isoduration_to_minute(history['worktime'])
+                    history['account_id'] = account_id
+                    history['username'] = self._get_username(account_id)
+                    history["aw_plans"] = 0.0
+                    history["aw_results"] = 0.0
 
-            for history in histories:
-                history['af_time'] = annofabcli.utils.isoduration_to_minute(history['worktime'])
-                username = self._get_username(account_id)
-                history['account_id'] = account_id
-                history['username'] = username
-                history["aw_plans"] = 0.0
-                history["aw_results"] = 0.0
+                all_histories.extend(histories)
+            else:
+                user_id_bool_list = [user_id_search in self._get_user_id(account_id).upper().upper() for user_id_search in self.user_id_search_list]
+                if True in user_id_bool_list:
+                    for history in histories:
+                        history['af_time'] = annofabcli.utils.isoduration_to_minute(history['worktime'])
+                        history['account_id'] = account_id
+                        history['username'] = self._get_username(account_id)
+                        history["aw_plans"] = 0.0
+                        history["aw_results"] = 0.0
 
-            all_histories.extend(histories)
+                    all_histories.extend(histories)
+
         return all_histories
 
     def create_afaw_time_df(self) -> Tuple[List[Any], List[Any]]:
@@ -127,7 +155,8 @@ class ListLaborWorktime(AbstractCommandLineInterface):
     """
     労務管理画面の作業時間を出力する
     """
-    def list_labor_worktime(self, project_id: str):
+
+    def list_labor_worktime(self, project_id: str, user_id_list: List[str]):
         """
         """
 
@@ -137,7 +166,7 @@ class ListLaborWorktime(AbstractCommandLineInterface):
         organization_id = get_organization_id_from_project_id(self.service, project_id)
         database = Database(self.service, project_id, organization_id)
         # Annofabから取得した情報に関するデータベースを取得するクラス
-        table_obj = FutureTable(database=database)
+        table_obj = FutureTable(database=database, user_id_list=user_id_list)
         # Databaseから取得した情報を元にPandas DataFrameを生成するクラス
         #     チェックポイントファイルがあること前提
         return table_obj.create_afaw_time_df()
@@ -147,22 +176,29 @@ class ListLaborWorktime(AbstractCommandLineInterface):
         start_date = datetime.datetime.strptime(args.start_date, '%Y-%m-%d').date()
         end_date = datetime.datetime.strptime(args.end_date, '%Y-%m-%d').date()
         date_list = date_range(start_date, end_date)
+        user_id_list = args.user_id
 
         afaw_time_list = []
         project_members_list = []
         for i, project_id in enumerate(args.project_id):
             logger.debug(f"{i + 1} 件目: project_id = {project_id}")
-            afaw_time_df, project_members = self.list_labor_worktime(project_id)
+            afaw_time_df, project_members = self.list_labor_worktime(project_id, user_id_list)
             afaw_time_list.append(afaw_time_df)
             project_members_list.extend(project_members)
-        print_time_list = print_time_list_from_work_time_list(list(set(project_members_list)), afaw_time_list,
-                                                              date_list)
+        print_time_list, print_total_time = print_time_list_from_work_time_list(list(set(project_members_list)),
+                                                                                afaw_time_list,
+                                                                                date_list)
 
         output_lines: List[str] = []
         output_lines.append(f"Start: , {start_date},  End: , {end_date}")
+        output_lines.append("project_title: ," + ",".join([self.facade.get_project_title(p) for p in args.project_id]))
         output_lines.extend([",".join([str(cell) for cell in row]) for row in print_time_list])
+        output_lines.append("total_aw_plans: ," + str(print_total_time["total_aw_plans"]))
+        output_lines.append("total_aw_results: ," + str(print_total_time["total_aw_results"]))
+        output_lines.append("total_af_time: ," + str(print_total_time["total_af_time"]))
+        output_lines.append("total_diff: ," + str(print_total_time["total_diff"]))
+        output_lines.append("total_diff_per: ," + str(print_total_time["total_diff_per"]))
         annofabcli.utils.output_string("\n".join(output_lines), args.output)
-
 
 def main(args):
     service = build_annofabapi_resource_and_login()
@@ -172,11 +208,12 @@ def main(args):
 
 def parse_args(parser: argparse.ArgumentParser):
     argument_parser = ArgumentParser(parser)
-
     parser.add_argument(
         '-p', '--project_id', type=str, required=True, nargs='+',
-        help="集計対象のプロジェクトのproject_idを指定します。複数指定した場合は合計値を出力します。"
-        "`file://`を先頭に付けると、project_idの一覧が記載されたファイルを指定できます。")
+        help="集計対象のプロジェクトのproject_idを指定します。複数指定した場合は合計値を出力します。")
+    parser.add_argument(
+        '-u', '--user_id', type=str, nargs='+', default=None, help='集計対象のユーザのuser_idに部分一致するものを集計します。'
+                                                                   '指定しない場合は、プロジェクトメンバが指定されます。')
     parser.add_argument("--start_date", type=str, required=True, help="集計開始日(%%Y-%%m-%%d)")
     parser.add_argument("--end_date", type=str, required=True, help="集計終了日(%%Y-%%m-%%d)")
 
--- annofabcli/experimental/utils.py
+++ annofabcli/experimental/utils.py
@@ -19,6 +19,9 @@ def print_time_list_from_work_time_list(username_list: list, work_time_lists: li
     new_header_item_list.append("date / time")
     new_footer_item_list.append("total_time")
     username_footer_list: Dict[str, Any] = {}
+    total_aw_plans = 0.0
+    total_aw_results = 0.0
+    total_af_time = 0.0
 
     for username in username_list:
         new_header_name_list.append(username)
@@ -69,18 +72,26 @@ def print_time_list_from_work_time_list(username_list: list, work_time_lists: li
 
     for username in username_list:
         new_footer_item_list.append(round(username_footer_list[username]["aw_plans"], 2))
+        total_aw_plans += username_footer_list[username]["aw_plans"]
         new_footer_item_list.append(round(username_footer_list[username]["aw_results"], 2))
+        total_aw_results += username_footer_list[username]["aw_results"]
         new_footer_item_list.append(round(username_footer_list[username]["af_time"], 2))
+        total_af_time += username_footer_list[username]["af_time"]
         total_diff = round(username_footer_list[username]["aw_results"] - username_footer_list[username]["af_time"], 2)
         total_diff_per = round(total_diff / username_footer_list[username]["aw_results"], 2) \
             if (username_footer_list[username]["aw_results"] != 0.0) else 0.0
         new_footer_item_list.append(total_diff)
         new_footer_item_list.append(total_diff_per)
 
+
+
     print_time_list.append(new_footer_item_list)
 
-    return print_time_list
-
+    return print_time_list,{"total_aw_results":round(total_aw_results,2),
+                            "total_aw_plans":round(total_aw_plans,2),
+                            "total_af_time":round(total_af_time,2),
+                           "total_diff":round(total_aw_results - total_af_time,2),
+    "total_diff_per":(round( (total_aw_results - total_af_time) / total_aw_results, 2) if (total_aw_results != 0.00) else 0.00 )}
 
 def print_time_list_csv(print_time_list: list) -> None:
     for time_list in print_time_list:
