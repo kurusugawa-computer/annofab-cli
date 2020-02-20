1538f330ffc11f1168e4e3cffb5ad03444498607
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-02-20 11:34:21 +0900

    format

--- annofabcli/instruction/copy_instruction.py
+++ annofabcli/instruction/copy_instruction.py
@@ -1,12 +1,12 @@
 import argparse
 import logging.handlers
-import time
-import uuid
+import mimetypes
 from pathlib import Path
+from typing import Optional
 
-import pyquery
-from annofabapi.models import OrganizationMember, ProjectMember, ProjectMemberRole
-from annofabapi.utils import download
+from annofabapi.models import ProjectMemberRole
+from pyquery import PyQuery
+
 import annofabcli
 from annofabcli import AnnofabApiFacade
 from annofabcli.common.cli import AbstractCommandLineInterface, ArgumentParser, build_annofabapi_resource_and_login
@@ -18,6 +18,7 @@ class CopyInstruction(AbstractCommandLineInterface):
     """
     作業ガイドをコピーする。
     """
+
     def validate_projects(self, src_project_id: str, dest_project_id: str):
         """
         適切なRoleが付与されているかを確認する。
@@ -28,59 +29,63 @@ class CopyInstruction(AbstractCommandLineInterface):
         super().validate_project(src_project_id, project_member_roles=None)
         super().validate_project(dest_project_id, project_member_roles=[ProjectMemberRole.OWNER])
 
-
-    def upload_html_to_instruction(self, project_id: str, html_path: Path):
-        pq_html = pyquery.PyQuery(filename=str(html_path))
-        pq_img = pq_html("img")
-
-        # 画像をすべてアップロードして、img要素のsrc属性値を annofab urlに変更する
-        for img_elm in pq_img:
-            src_value: str = img_elm.attrib.get("src")
-            if src_value is None:
-                continue
-
-            if src_value.startswith("http:") or src_value.startswith("https:") or src_value.startswith("data:"):
-                continue
-
-            if src_value[0] == "/":
-                img_path = Path(src_value)
-            else:
-                img_path = html_path.parent / src_value
-
-            if img_path.exists():
-                image_id = str(uuid.uuid4())
-                img_url = self.service.wrapper.upload_instruction_image(project_id, image_id, str(img_path))
-
-                logger.debug(f"image uploaded. file={img_path}, instruction_image_url={img_url}")
-                img_elm.attrib["src"] = img_url
-                time.sleep(1)
-
-            else:
-                logger.warning(f"image does not exist. path={img_path}")
-
-        # 作業ガイドの更新(body element)
-        html_data = pq_html("body").html()
-        self.update_instruction(project_id, html_data)
-        logger.info("作業ガイドを更新しました。")
-
     @staticmethod
     def get_instruction_image_id_from_url(url: str):
         # URL Queryを除いたURLを取得する
         url_without_query = url.split("?")[0]
         return url_without_query.split("/")[-1]
 
+    @staticmethod
+    def _get_mime_type_from_filename(filename: Optional[str]) -> str:
+        """
+        ファイル名からMIME TYPEを取得する。
+        """
+        DEFAULT_MIME_TYPE = "application/octet-stream"
+        if filename is None:
+            return DEFAULT_MIME_TYPE
 
+        content_type = mimetypes.guess_type(filename)[0]
+        return content_type if content_type is not None else DEFAULT_MIME_TYPE
 
-    def upload_instruction_image(self, src_instruction_image_url: str, project_id: str, temp_dir:Path):
-        src_instruction_image_url
+    def upload_instruction_image(self, src_project_id: str, dest_project_id: str, pq_img: PyQuery) -> Optional[str]:
+        """
+        コピー元の作業ガイド画像を、コピー先にアップロードする。
+        Args:
+            src_project_id:
+            dest_project_id:
+            pq_img:
+
+        Returns:
+
+        """
+
+        src_instruction_image_url: str = pq_img.attr["src"]
+        if src_instruction_image_url is None:
+            logger.warning(f"{pq_img} にsrc属性がないのでスキップします。")
+            return None
+
+        logger.debug(f"コピー元プロジェクトの {src_instruction_image_url} を、コピー先プロジェクトにアップロードします。")
         instruction_image_id = self.get_instruction_image_id_from_url(src_instruction_image_url)
 
-        dest_file = temp_dir / instruction_image_id
+        # alt属性値にファイル名が設定されている
+        content_type = self._get_mime_type_from_filename(pq_img.attr["alt"])
 
-        img_url = self.service.wrapper.upload_instruction_image(project_id, image_id, str(img_path))
+        response_image = self.service.api._request_get_with_cookie(src_project_id, src_instruction_image_url)
 
+        dest_instruction_image_url = self.service.wrapper.upload_file_object_as_instruction_image(
+            dest_project_id, instruction_image_id, fp=response_image.content, content_type=content_type
+        )
+        return dest_instruction_image_url
 
-    def register_instruction(self, project_id: str, instruction_html: str):
+    def put_instruction(self, project_id: str, instruction_html: str):
+        old_instruction = self.service.wrapper.get_latest_instruction(project_id)
+        request_body = {
+            "html": instruction_html,
+            "last_updated_datetime": old_instruction["last_updated_datetime"] if old_instruction is not None else None,
+        }
+        self.service.api.put_instruction(project_id, request_body=request_body)
+
+    def register_instruction(self, src_project_id: str, dest_project_id: str, instruction_html: str):
         """
         作業ガイド用HTMLを、作業ガイドとして登録する。
         作業ガイドHTMLに記載されている画像はダウンロードする。
@@ -93,60 +98,32 @@ class CopyInstruction(AbstractCommandLineInterface):
 
         """
 
-        pq_html = pyquery.PyQuery(instruction_html)
-        pq_img = pq_html("img")
+        pq_html = PyQuery(instruction_html)
+        pq_img_list = pq_html("img")
 
-        # コピー画像をすべてアップロードして、img要素のsrc属性値を annofab urlに変更する
-        for img_elm in pq_img:
-            src_value: str = img_elm.attrib.get("src")
-            if src_value is None:
-                logger.warning(f"{img_elm} にsrc属性がないのでスキップします。")
-                continue
+        for img_elm in pq_img_list:
+            pq_img = PyQuery(img_elm)
 
-            download()
-            if src_value.startswith("http:") or src_value.startswith("https:") or src_value.startswith("data:"):
-                continue
+            dest_instruction_image_url = self.upload_instruction_image(src_project_id, dest_project_id, pq_img)
+            if dest_instruction_image_url is not None:
+                pq_img.attr["src"] = dest_instruction_image_url
 
-            if src_value[0] == "/":
-                img_path = Path(src_value)
-            else:
-                img_path = html_path.parent / src_value
+        self.put_instruction(dest_project_id, str(pq_html))
 
-            if img_path.exists():
-                image_id = str(uuid.uuid4())
-                img_url = self.service.wrapper.upload_instruction_image(project_id, image_id, str(img_path))
-
-                logger.debug(f"image uploaded. file={img_path}, instruction_image_url={img_url}")
-                img_elm.attrib["src"] = img_url
-                time.sleep(1)
-
-            else:
-                logger.warning(f"image does not exist. path={img_path}")
-
-
-
-
-
-    def copy_instruction(self, src_project_id: str, dest_project_id:str, temp_dir: Path):
+    def copy_instruction(self, src_project_id: str, dest_project_id: str, temp_dir: Path):
         self.validate_projects(src_project_id, dest_project_id)
+        src_project_title = self.facade.get_project_title(src_project_id)
+        dest_project_title = self.facade.get_project_title(dest_project_id)
 
-        instruction = self.service.wrapper.get_latest_instruction(src_project_id)
-        if instruction is None:
-            logger.warning(f"作業ガイドが登録されていないので、コピーしません。")
+        src_instruction = self.service.wrapper.get_latest_instruction(src_project_id)
+        if src_instruction is None:
+            logger.warning(f"コピー元プロジェクト '{src_project_title}' に作業ガイドが設定されていません。終了します。")
             return
 
-        temp_dir.mkdir(parents=True, exist_ok=True)
+        if not self.confirm_processing(f"'{src_project_title}' の作業ガイドを、'{dest_project_title}' にコピーしますか？"):
+            return
 
-        html =
-
-
-        if len(histories) > 0:
-            last_updated_datetime = histories[0]["updated_datetime"]
-        else:
-            last_updated_datetime = None
-
-        request_body = {"html": html_data, "last_updated_datetime": last_updated_datetime}
-        self.service.api.put_instruction(project_id, request_body=request_body)
+        self.register_instruction(src_project_id, dest_project_id, instruction_html=src_instruction["html"])
 
     def main(self):
         args = self.args
@@ -164,11 +141,11 @@ def parse_args(parser: argparse.ArgumentParser):
     parser.add_argument("src_project_id", type=str, help="コピー元のプロジェクトのproject_id")
     parser.add_argument("dest_project_id", type=str, help="コピー先のプロジェクトのproject_id")
 
-    parser.add_argument("--temp_dir", type=str, required=True, help="temporaryディレクトリのパス。コピー元からダウンロードした作業ガイド画像を一時的に保存する。")
-
+    parser.add_argument(
+        "--temp_dir", type=str, required=True, help="temporaryディレクトリのパス。コピー元からダウンロードした作業ガイド画像を一時的に保存する。"
+    )
 
     parser.set_defaults(subcommand_func=main)
-
 
     argument_parser = ArgumentParser(parser)
 
