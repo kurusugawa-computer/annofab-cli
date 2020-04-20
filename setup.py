15172e754d38bb2755b7ccb58137f6cce83e1b47
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-04-20 13:42:52 +0900

    update lock

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     keywords="annofab api cli",
     url="https://github.com/kurusugawa-computer/annofab-cli",
     install_requires=[
-        "annofabapi>=0.30.2",
+        "annofabapi>=0.30.3",
         "requests",
         "pillow",
         "pyyaml",
