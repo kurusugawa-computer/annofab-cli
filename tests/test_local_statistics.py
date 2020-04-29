153a96efd54ac28975e034bc57857c1760b19063
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-04-29 13:46:12 +0900

    update test

--- tests/test_local_statistics.py
+++ tests/test_local_statistics.py
@@ -28,7 +28,6 @@ csv_obj = Csv(str(out_path), project_id)
 class TestTable:
     def test_get_task_history_df(self):
         task_history_df = pandas.read_csv(str(data_path / "statistics/task-history-df.csv"))
-        # task_df = pandas.read_csv(str(data_path / "statistics/タスクlist.csv"))
         task_df = pandas.DataFrame(
             {
                 "task_id": ["task1", "task2"],
@@ -84,7 +83,7 @@ class TestScatter:
         cls.scatter_obj = Scatter(outdir=str(out_path / "statistics"), project_id=project_id)
 
     def read_productivity_per_user(self):
-        productivity_per_user = pandas.read_csv("/vagrant_data/tmp/triad-13-メンバごとの生産性と品質.csv", header=[0, 1])
+        productivity_per_user = pandas.read_csv(str(data_path / "statistics/productivity-per-user.csv"), header=[0, 1])
         productivity_per_user.rename(
             columns={
                 "Unnamed: 0_level_1": "",
