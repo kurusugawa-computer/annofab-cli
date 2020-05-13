d5950de428af2266212db03968e8b90af60b1b28
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-05-14 01:09:18 +0900

    update Pipfile

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     keywords="annofab api cli",
     url="https://github.com/kurusugawa-computer/annofab-cli",
     install_requires=[
-        "annofabapi>=0.30.3",
+        "annofabapi>=0.33.0",
         "requests",
         "pillow",
         "pyyaml",
