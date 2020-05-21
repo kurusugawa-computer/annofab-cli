dff1d76c9408457ee12152ddb592d6b1155e7194
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-05-22 02:35:51 +0900

    __all__を利用する

--- annofabcli/__init__.py
+++ annofabcli/__init__.py
@@ -1,9 +1,14 @@
-# flake8: noqa: F401
-from annofabcli.common import enums
-from annofabcli.common import typing
-from annofabcli.common import exceptions
-from annofabcli.common import utils
+from annofabcli.common import cli, enums, exceptions, typing, utils
 from annofabcli.common.facade import AnnofabApiFacade
-from annofabcli.common import cli
 
 from .__version__ import __version__
+
+__all__ = [
+    "AnnofabApiFacade",
+    "cli",
+    "enums",
+    "exceptions",
+    "typing",
+    "utils",
+    "__version__",
+]
