7497b380cfc6dc5d00481f5923a7f4ea5b701b5c
unarist <m.unarist@gmail.com>
2020-06-23 02:34:27 +0900

    [supplementary put] 補助情報をCSVで登録するコマンドを追加

--- annofabcli/supplementary/subcommand_supplementary.py
+++ annofabcli/supplementary/subcommand_supplementary.py
@@ -3,6 +3,7 @@ import argparse
 import annofabcli
 import annofabcli.common.cli
 import annofabcli.supplementary.list_supplementary_data
+import annofabcli.supplementary.put_supplementary_data
 
 
 def parse_args(parser: argparse.ArgumentParser):
@@ -11,6 +12,7 @@ def parse_args(parser: argparse.ArgumentParser):
 
     # サブコマンドの定義
     annofabcli.supplementary.list_supplementary_data.add_parser(subparsers)
+    annofabcli.supplementary.put_supplementary_data.add_parser(subparsers)
 
 
 def add_parser(subparsers: argparse._SubParsersAction):
