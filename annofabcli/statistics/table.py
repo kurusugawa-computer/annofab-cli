4869b79cff364a5e7e4f0f04008577aa8f78030f
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-09-04 16:10:38 +0900

    [visualize statistics] 実績作業時間が入力されていないプロジェクトでも出力できるようにする

--- annofabcli/statistics/table.py
+++ annofabcli/statistics/table.py
@@ -1258,9 +1258,13 @@ class Table:
 
         if len(df_labor) > 0:
             df_agg_labor = df_labor.pivot_table(values="worktime_result_hour", index="user_id", aggfunc=numpy.sum)
-            df_agg_labor["last_working_date"] = df_labor[df_labor["worktime_result_hour"] > 0].pivot_table(
+            df_tmp = df_labor[df_labor["worktime_result_hour"] > 0].pivot_table(
                 values="date", index="user_id", aggfunc=numpy.max
             )
+            if len(df_tmp) > 0:
+                df_agg_labor["last_working_date"] = df_tmp
+            else:
+                df_agg_labor["last_working_date"] = numpy.nan
             df = df_agg_task_history.join(df_agg_labor)
         else:
             df = df_agg_task_history
@@ -1390,11 +1394,12 @@ class Table:
             df_agg_labor = df_labor.pivot_table(
                 values=["worktime_result_hour"], index="date", aggfunc=numpy.sum
             ).fillna(0)
-            df_agg_labor["working_user_count"] = (
-                df_labor[df_labor["worktime_result_hour"] > 0]
-                .pivot_table(values=["user_id"], index="date", aggfunc="count")
-                .fillna(0)
-            )
+            df_tmp = df_labor[df_labor["worktime_result_hour"] > 0].pivot_table(values=["user_id"], index="date", aggfunc="count").fillna(0)
+
+            if len(df_tmp) > 0:
+                df_agg_labor["working_user_count"] = df_tmp
+            else:
+                df_agg_labor["working_user_count"] = 0
         else:
             df_agg_labor = pandas.DataFrame(columns=["worktime_result_hour", "working_user_count"])
 
