b4345264431307cc82f38abdec55d1475838cea1
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-10-20 23:38:46 +0900

    format

--- annofabcli/input_data/put_input_data.py
+++ annofabcli/input_data/put_input_data.py
@@ -225,14 +225,16 @@ class PutInputData(AbstractCommandLineInterface):
         df_duplicated_input_data_name = df[df["input_data_name"].duplicated()]
         if len(df_duplicated_input_data_name) > 0:
             logger.error(
-                f"{csv_path}に記載されている`input_data_name`が重複しています。\n{df_duplicated_input_data_name['input_data_name'].to_string(index=False)}"
+                f"{csv_path}に記載されている`input_data_name`が重複しています。\n"
+                f"{df_duplicated_input_data_name['input_data_name'].to_string(index=False)}"
             )
             raise RuntimeError(f"{csv_path}に記載されている`input_data_name`が重複しています。")
 
         df_duplicated_input_data_path = df[df["input_data_path"].duplicated()]
         if len(df_duplicated_input_data_path) > 0:
             logger.error(
-                f"{csv_path}に記載されている`input_data_path`が重複しています。\n{df_duplicated_input_data_name['input_data_path'].to_string(index=False)}"
+                f"{csv_path}に記載されている`input_data_path`が重複しています。\n"
+                f"{df_duplicated_input_data_name['input_data_path'].to_string(index=False)}"
             )
             raise RuntimeError(f"{csv_path}に記載されている`input_data_path`が重複しています。")
 
