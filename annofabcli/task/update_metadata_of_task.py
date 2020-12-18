24c12d9c25daaa4e5c1d1497a64ca05351178fe3
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-12-18 17:50:52 +0900

    [task update_metadata] コマンドライン引数に`--batch_size`オプションを追加

--- annofabcli/task/update_metadata_of_task.py
+++ annofabcli/task/update_metadata_of_task.py
@@ -1,6 +1,6 @@
 import argparse
 import logging
-from typing import Dict, List, Union
+from typing import Dict, List, Optional, Union
 
 import annofabapi
 from annofabapi.models import ProjectMemberRole
@@ -26,15 +26,23 @@ class UpdateMetadataOfTaskMain(AbstracCommandCinfirmInterface):
         AbstracCommandCinfirmInterface.__init__(self, all_yes)
 
     def update_metadata_of_task(
-        self,
-        project_id: str,
-        task_id_list: List[str],
-        metadata: Metadata,
+        self, project_id: str, task_id_list: List[str], metadata: Metadata, batch_size: Optional[int] = None
     ):
-        logger.info(f"{len(task_id_list)} 件のタスクのmetadataを、{metadata} に変更します。")
+        if batch_size is None:
+            logger.info(f"{len(task_id_list)} 件のタスクのmetadataを、{metadata} に変更します。")
+            request_body = {task_id: metadata for task_id in task_id_list}
+            self.service.api.patch_tasks_metadata(project_id, request_body=request_body)
 
-        request_body = {task_id: metadata for task_id in task_id_list}
-        self.service.api.patch_tasks_metadata(project_id, request_body=request_body)
+        else:
+            logger.info(f"{len(task_id_list)} 件のタスクのmetadataを{metadata} に、{batch_size}個ずつ変更します。")
+            first_index = 0
+            while first_index < len(task_id_list):
+                logger.info(
+                    f"{first_index+1} 〜 {min(first_index+batch_size, len(task_id_list)-1)} 件目のタスクのmetadataを更新します。"
+                )
+                request_body = {task_id: metadata for task_id in task_id_list[first_index:batch_size]}
+                self.service.api.patch_tasks_metadata(project_id, request_body=request_body)
+                first_index += batch_size
 
 
 class UpdateMetadataOfTask(AbstractCommandLineInterface):
@@ -44,7 +52,9 @@ class UpdateMetadataOfTask(AbstractCommandLineInterface):
         metadata = annofabcli.common.cli.get_json_from_args(args.metadata)
         super().validate_project(args.project_id, [ProjectMemberRole.OWNER, ProjectMemberRole.TRAINING_DATA_USER])
         main_obj = UpdateMetadataOfTaskMain(self.service)
-        main_obj.update_metadata_of_task(args.project_id, task_id_list=task_id_list, metadata=metadata)
+        main_obj.update_metadata_of_task(
+            args.project_id, task_id_list=task_id_list, metadata=metadata, batch_size=args.batch_size
+        )
 
 
 def main(args):
@@ -64,6 +74,14 @@ def parse_args(parser: argparse.ArgumentParser):
         type=str,
         help="タスクに設定する`metadata`をJSON形式で指定してください。メタデータの値には文字列、数値、真偽値のいずれかを指定してください。"
         "`file://`を先頭に付けると、JSON形式のファイルを指定できます。",
+    )
+
+    parser.add_argument(
+        "--batch_size",
+        required=False,
+        default=100,
+        type=int,
+        help="タスクのメタデータを何個ごとに更新するかを指定してください。一度に更新するタスクが多いとタイムアウトが発生する恐れがあります。",
     )
 
     parser.set_defaults(subcommand_func=main)
