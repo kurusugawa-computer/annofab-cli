372ce6be9f837bdb25b68512f671a123a71d9df9
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-12-23 14:15:09 +0900

    update

---
+++ annofabcli/stat_visualization/subcommand_statistics.py
@@ -0,0 +1,42 @@
+import argparse
+
+import annofabcli
+import annofabcli.common.cli
+import annofabcli.statistics.list_annotation_count
+import annofabcli.statistics.list_by_date_user
+import annofabcli.statistics.list_cumulative_labor_time
+import annofabcli.statistics.list_labor_time_per_user
+import annofabcli.statistics.list_task_progress
+import annofabcli.statistics.merge_visualization_dir
+import annofabcli.statistics.summarize_task_count
+import annofabcli.statistics.summarize_task_count_by_task_id
+import annofabcli.statistics.summarize_task_count_by_user
+import annofabcli.statistics.visualize_statistics
+
+
+def parse_args(parser: argparse.ArgumentParser):
+
+    subparsers = parser.add_subparsers(dest="subcommand_name")
+
+    # サブコマンドの定義
+    annofabcli.statistics.list_annotation_count.add_parser(subparsers)
+    annofabcli.statistics.list_cumulative_labor_time.add_parser(subparsers)
+    annofabcli.statistics.list_labor_time_per_user.add_parser(subparsers)
+    annofabcli.statistics.list_by_date_user.add_parser(subparsers)
+    annofabcli.statistics.list_task_progress.add_parser(subparsers)
+    annofabcli.statistics.merge_visualization_dir.add_parser(subparsers)
+    annofabcli.statistics.summarize_task_count.add_parser(subparsers)
+    annofabcli.statistics.summarize_task_count_by_task_id.add_parser(subparsers)
+    annofabcli.statistics.summarize_task_count_by_user.add_parser(subparsers)
+    annofabcli.statistics.visualize_statistics.add_parser(subparsers)
+
+
+def add_parser(subparsers: argparse._SubParsersAction):
+    subcommand_name = "stat_visualization"
+    subcommand_help = "`annofabcli statistics visualization`コマンドの出力結果を加工するサブコマンド"
+    description = "`annofabcli statistics visualization`コマンドの出力結果を加工するサブコマンド"
+
+    parser = annofabcli.common.cli.add_parser(
+        subparsers, subcommand_name, subcommand_help, description, is_subcommand=False
+    )
+    parse_args(parser)
---
+++ annofabcli/stat_visualization/write_performance_rating_csv.py
@@ -0,0 +1,467 @@
+from typing import List, Any, Dict
+import annofabcli
+from annofabcli.common.cli import (
+    AbstractCommandLineInterface,
+    ArgumentParser,
+    build_annofabapi_resource_and_login,
+    get_json_from_args,
+    get_wait_options_from_args,
+)
+from annofabcli.statistics.csv import FILENAME_PEFORMANCE_PER_USER
+import argparse
+import logging
+from annofabcli import AnnofabApiFacade
+import os
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
+import annofabapi
+import numpy
+import pandas
+from annofabcli.common.utils import print_csv, read_lines_except_blank_line, read_multiheader_csv
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class ResultDataframe:
+    annotation_productivity: pandas.DataFrame
+    inspection_acceptance_productivity: pandas.DataFrame
+    quality_per_task: pandas.DataFrame
+    quality_per_annotation: pandas.DataFrame
+
+
+class WriteRatingCsv:
+    def __init__(self, service: annofabapi.Resource):
+        self.service = service
+
+    def _create_organization_member_df(self) -> pandas.DataFrame:
+        organization_list = read_lines_except_blank_line(
+            f"{os.path.dirname(os.path.abspath(__file__))}/../../resources/organization_name.txt"
+        )
+        organization_member_list = []
+        for organization in organization_list:
+            organization_member_list.extend(self.service.wrapper.get_all_organization_members(organization))
+
+        df = pandas.DataFrame(organization_member_list)
+        df = df.drop_duplicates(subset=["user_id"])
+        df = df[["user_id", "username", "biography"]]
+        df.columns = pandas.MultiIndex.from_tuples([("user_id", ""), ("username", ""), ("biography", "")])
+        return df
+
+
+
+def _get_member_perfomance_csv(project_dir: Path) -> Path:
+    return Path(f"{str(project_dir)}/メンバごとの生産性と品質.csv")
+
+
+def join_annotation_productivity(
+    df: pandas.DataFrame,
+    df_performance: pandas.DataFrame,
+    project_title: str,
+    threshold_worktime: float,
+    threshold_task_count: int,
+) -> pandas.DataFrame:
+    df_joined = df_performance
+    df_joined = df_joined.set_index("user_id")
+
+    df_joined = df_joined[
+        (df_joined[("task_count", "annotation")] >= threshold_task_count)
+        | (df_joined[("prediction_actual_worktime_hour", "annotation")] >= threshold_worktime)
+    ]
+    df_tmp = df_joined[[("actual_worktime/annotation_count", "annotation")]]
+
+    df_tmp.columns = pandas.MultiIndex.from_tuples([(project_title, "actual_worktime/annotation_count__annotation")])
+    return df.join(df_tmp)
+
+
+def join_inspection_acceptance_productivity(
+    df: pandas.DataFrame,
+    df_performance: pandas.DataFrame,
+    project_title: str,
+    threshold_worktime: float,
+    threshold_task_count: int,
+) -> pandas.DataFrame:
+    def _join_inspection():
+        if ("actual_worktime/annotation_count", "inspection") not in df_performance.columns:
+            return df
+
+        df_joined = df_performance[
+            (df_performance[("task_count", "inspection")] >= threshold_task_count)
+            | (df_performance[("prediction_actual_worktime_hour", "inspection")] >= threshold_worktime)
+        ]
+        df_joined.set_index("user_id", inplace=True)
+        df_tmp = df_joined[[("actual_worktime/annotation_count", "inspection")]]
+        df_tmp.columns = pandas.MultiIndex.from_tuples(
+            [(project_title, "actual_worktime/annotation_count__inspection")]
+        )
+        return df.join(df_tmp)
+
+    def _join_acceptance():
+        if ("actual_worktime/annotation_count", "acceptance") not in df_performance.columns:
+            return df
+
+        df_joined = df_performance[
+            (df_performance[("task_count", "acceptance")] >= threshold_task_count)
+            | (df_performance[("prediction_actual_worktime_hour", "acceptance")] >= threshold_worktime)
+        ]
+        df_joined.set_index("user_id", inplace=True)
+        df_tmp = df_joined[[("actual_worktime/annotation_count", "acceptance")]]
+        df_tmp.columns = pandas.MultiIndex.from_tuples(
+            [(project_title, "actual_worktime/annotation_count__acceptance")]
+        )
+        return df.join(df_tmp)
+
+    df = _join_inspection()
+    df = _join_acceptance()
+
+    return df
+
+
+def join_quality_per_task(
+    df: pandas.DataFrame,
+    df_performance: pandas.DataFrame,
+    project_title: str,
+    threshold_worktime: float,
+    threshold_task_count: int,
+) -> pandas.DataFrame:
+    df_joined = df_performance
+    df_joined = df_joined.set_index("user_id")
+
+    df_joined = df_joined[
+        (df_joined[("task_count", "annotation")] >= threshold_task_count)
+        | (df_joined[("prediction_actual_worktime_hour", "annotation")] >= threshold_worktime)
+    ]
+    df_tmp = df_joined[[("rejected_count/task_count", "annotation")]]
+
+    df_tmp.columns = pandas.MultiIndex.from_tuples([(project_title, "rejected_count/task_count")])
+    return df.join(df_tmp)
+
+
+def join_quality_per_annotation(
+    df: pandas.DataFrame,
+    df_performance: pandas.DataFrame,
+    project_title: str,
+    threshold_worktime: float,
+    threshold_task_count: int,
+) -> pandas.DataFrame:
+    df_joined = df_performance
+    df_joined = df_joined.set_index("user_id")
+
+    df_joined = df_joined[
+        (df_joined[("task_count", "annotation")] >= threshold_task_count)
+        | (df_joined[("prediction_actual_worktime_hour", "annotation")] >= threshold_worktime)
+    ]
+    df_tmp = df_joined[[("pointed_out_inspection_comment_count/annotation_count", "annotation")]]
+
+    df_tmp.columns = pandas.MultiIndex.from_tuples(
+        [(project_title, "pointed_out_inspection_comment_count/annotation_count")]
+    )
+    return df.join(df_tmp)
+
+
+def create_rating_df(
+    df_user: pandas.DataFrame, target_dir: Path, threshold_worktime: float, threshold_task_count: int
+) -> ResultDataframe:
+
+    df_annotation_productivity = df_user
+    df_inspection_acceptance_productivity = df_user
+    df_quality_per_task = df_user
+    df_quality_per_annotation = df_user
+
+    for project_dir in target_dir.iterdir():
+        if not project_dir.is_dir():
+            continue
+
+        csv = _get_member_perfomance_csv(project_dir)
+        project_title = project_dir.name
+        if not csv.exists():
+            logger.warning(f"{csv} は存在しないのでスキップします。")
+            continue
+
+        df_performance = read_multiheader_csv(str(csv), header_row_count=2)
+        df_annotation_productivity = join_annotation_productivity(
+            df_annotation_productivity,
+            df_performance,
+            project_title=project_title,
+            threshold_worktime=threshold_worktime,
+            threshold_task_count=threshold_task_count,
+        )
+        df_inspection_acceptance_productivity = join_inspection_acceptance_productivity(
+            df_inspection_acceptance_productivity,
+            df_performance,
+            project_title=project_title,
+            threshold_worktime=threshold_worktime,
+            threshold_task_count=threshold_task_count,
+        )
+        df_quality_per_task = join_quality_per_task(
+            df_quality_per_task,
+            df_performance,
+            project_title=project_title,
+            threshold_worktime=threshold_worktime,
+            threshold_task_count=threshold_task_count,
+        )
+        df_quality_per_annotation = join_quality_per_annotation(
+            df_quality_per_annotation,
+            df_performance,
+            project_title=project_title,
+            threshold_worktime=threshold_worktime,
+            threshold_task_count=threshold_task_count,
+        )
+
+    return ResultDataframe(
+        annotation_productivity=df_annotation_productivity.reset_index(),
+        inspection_acceptance_productivity=df_inspection_acceptance_productivity.reset_index(),
+        quality_per_task=df_quality_per_task.reset_index(),
+        quality_per_annotation=df_quality_per_annotation.reset_index(),
+    )
+
+
+def output_csv(result: ResultDataframe, output_dir: Path):
+    print_csv(result.annotation_productivity, str(output_dir / "annotation_productivity.csv"))
+    print_csv(result.inspection_acceptance_productivity, str(output_dir / "inspection_acceptance_productivity.csv"))
+    print_csv(result.quality_per_task, str(output_dir / "quality_per_task.csv"))
+    print_csv(result.quality_per_annotation, str(output_dir / "quality_per_annotation.csv"))
+
+
+def to_rank(series: pandas.Series) -> pandas.Series:
+    quantile = list(series.quantile([0.25, 0.5, 0.75]))
+
+    def _to_point(value) -> int:
+        if numpy.isnan(value):
+            return value
+        elif value < quantile[0]:
+            return 4
+        elif quantile[0] <= value < quantile[1]:
+            return 3
+        elif quantile[1] <= value < quantile[2]:
+            return 2
+        elif quantile[2] <= value:
+            return 1
+        else:
+            return value
+
+    if len([v for v in series if not numpy.isnan(v)]) >= 4:
+        return series.map(_to_point)
+    else:
+        return pandas.Series([numpy.nan] * len(series))
+
+
+def to_deviation(series: pandas.Series, threshold_deviation_user_count: Optional[int] = None) -> pandas.Series:
+    if series.count() == 0 or (
+        threshold_deviation_user_count is not None and series.count() < threshold_deviation_user_count
+    ):
+        return pandas.Series([numpy.nan] * len(series))
+    else:
+        std = series.std(ddof=0)
+        mean = series.mean()
+        if std != 0.0:
+            return series.map(lambda x: (x - mean) / std * 10 + 50)
+        else:
+            return series.map(lambda x: 50 if not numpy.isnan(x) else numpy.nan)
+
+
+def create_rank_df(df: pandas.DataFrame) -> pandas.DataFrame:
+    df_rank = df.copy()
+    for col in df.columns[3:]:
+        df_rank[col] = to_rank(df[col])
+    return df_rank
+
+
+def create_deviation_df(df: pandas.DataFrame, threshold_deviation_user_count: Optional[int] = None) -> pandas.DataFrame:
+    df_rank = df.copy()
+    user_columns = df.columns[0:3]
+    project_columns = df.columns[3:]
+    for col in project_columns:
+        df_rank[col] = to_deviation(df[col], threshold_deviation_user_count)
+
+    df_rank["mean_of_deviation"] = df_rank[project_columns].mean(axis=1)
+    df_rank["count_of_project"] = df_rank[project_columns].count(axis=1)
+    return df_rank[list(user_columns) + [("mean_of_deviation", ""), ("count_of_project", "")] + list(project_columns)]
+
+
+def create_basic_statistics_df(df: pandas.DataFrame) -> pandas.DataFrame:
+    df_stat = df.describe().T
+    return df_stat
+
+
+def output_rank_csv(result: ResultDataframe, output_dir: Path):
+    print_csv(create_rank_df(result.annotation_productivity), str(output_dir / "annotation_productivity_rank.csv"))
+    print_csv(
+        create_rank_df(result.inspection_acceptance_productivity),
+        str(output_dir / "inspection_acceptance_productivity_rank.csv"),
+    )
+    print_csv(create_rank_df(result.quality_per_task), str(output_dir / "quality_per_task_rank.csv"))
+    print_csv(create_rank_df(result.quality_per_annotation), str(output_dir / "quality_per_annotation_rank.csv"))
+
+
+def output_deviation_csv(
+    result: ResultDataframe, output_dir: Path, threshold_deviation_user_count: Optional[int] = None
+):
+    """
+    偏差値に変換したものを出力する
+    """
+    df_annotation_productivity = create_deviation_df(result.annotation_productivity, threshold_deviation_user_count)
+    print_csv(
+        df_annotation_productivity,
+        str(output_dir / "annotation_productivity_deviation.csv"),
+    )
+    print_csv(
+        create_deviation_df(result.inspection_acceptance_productivity, threshold_deviation_user_count),
+        str(output_dir / "inspection_acceptance_productivity_deviation.csv"),
+    )
+    print_csv(
+        create_deviation_df(result.quality_per_task, threshold_deviation_user_count),
+        str(output_dir / "quality_per_task_deviation.csv"),
+    )
+
+    df_quality_per_annotation_deviation = create_deviation_df(
+        result.quality_per_annotation, threshold_deviation_user_count
+    )
+    print_csv(
+        df_quality_per_annotation_deviation,
+        str(output_dir / "quality_per_annotation_deviation.csv"),
+    )
+
+
+def output_basic_statistics_by_project(result: ResultDataframe, output_dir: Path):
+    """
+    プロジェクトごとの基本統計情報を出力する。
+    """
+    to_csv_kwargs = {"index": True}
+
+    print_csv(
+        create_basic_statistics_df(result.annotation_productivity),
+        str(output_dir / "annotation_productivity_summary.csv"),
+        to_csv_kwargs=to_csv_kwargs,
+    )
+    print_csv(
+        create_basic_statistics_df(result.inspection_acceptance_productivity),
+        str(output_dir / "inspection_acceptance_productivity_summary.csv"),
+        to_csv_kwargs=to_csv_kwargs,
+    )
+    print_csv(
+        create_basic_statistics_df(result.quality_per_task),
+        str(output_dir / "quality_per_task_summary.csv"),
+        to_csv_kwargs=to_csv_kwargs,
+    )
+    print_csv(
+        create_basic_statistics_df(result.quality_per_annotation),
+        str(output_dir / "quality_per_annotation_summary.csv"),
+        to_csv_kwargs=to_csv_kwargs,
+    )
+
+
+def parse_args():
+    parser = ArgumentParser(description="メンバごとのパフォーマンスを出力します。", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    return parser.parse_args()
+
+
+
+
+
+def create_user_df(target_dir: Path) -> pandas.DataFrame:
+    """
+    "メンバごとの生産性と品質.csv"から、ユーザのDataFrameを作成する。
+
+    Args:
+        target_dir:
+
+    Returns:
+        ユーザのDataFrame. columnは　("user_id", ""), ("username", ""), ("biography", "")
+
+    """
+    all_user_list: List[Dict[str,Any]] = []
+    for project_dir in target_dir.iterdir():
+        if not project_dir.is_dir():
+            continue
+
+        csv = project_dir / FILENAME_PEFORMANCE_PER_USER
+        if not csv.exists():
+            logger.warning(f"{csv} は存在しないのでスキップします。")
+            continue
+
+        tmp_df_user = read_multiheader_csv(str(csv), header_row_count=2)[[("user_id",""),("username",""),("biography","")]]
+        all_user_list.extend(tmp_df_user.to_dict("record"))
+
+    index = pandas.MultiIndex.from_tuples([
+        ("user_id", ""), ("username", ""), ("biography", "")
+    ])
+    df_user = pandas.DataFrame(all_user_list, columns=index)
+    df_user.drop_duplicates(inplace=True)
+    df_user.sort_values("user_id", inplace=True)
+    return df_user
+
+class WritePerformanceRatingCsv(AbstractCommandLineInterface):
+
+
+    def main(self) -> None:
+        args = self.args
+        # プロジェクトトップに移動する
+        now_dir = os.getcwd()
+        os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/../../")
+
+        target_dir: Path = args.dir
+        main_obj = WriteRatingCsv(annofabapi.build())
+        df_user = create_user_df(target_dir)
+
+        result = create_rating_df(
+            df_user, target_dir, threshold_worktime=args.threshold_worktime, threshold_task_count=args.threshold_task_count
+        )
+
+        output_dir: Path = args.output_dir
+        # output_csv(result, output_dir)
+        # output_rank_csv(result, output_dir)
+        output_deviation_csv(result, output_dir, threshold_deviation_user_count=args.threshold_deviation_user_count)
+        output_basic_statistics_by_project(result, output_dir)
+
+        # 移動前のディレクトリに戻る
+        os.chdir(now_dir)
+
+
+
+
+def parse_args(parser: argparse.ArgumentParser):
+    argument_parser = ArgumentParser(parser)
+
+    parser.add_argument("--dir", type=Path, required=True, help="プロジェクトディレクトリが存在するディレクトリを指定してください。プロジェクトディレクトリ内の`メンバごとの生産性と品質.csv`というファイルを読み込みます。")
+
+    parser.add_argument(
+        "--threshold_worktime",
+        type=int,
+        default=10.0,
+        help="作業時間の閾値。この時間未満の作業者は除外する。`threshold_task_count`とはOR条件で絞り込まれる。",
+    )
+    parser.add_argument(
+        "--threshold_task_count",
+        type=int,
+        default=10,
+        help="作業したタスク数の閾値。このタスク数未満の作業者は除外する。`threshold_worktime`とはOR条件で絞り込まれる。",
+    )
+    parser.add_argument(
+        "--threshold_deviation_user_count",
+        type=int,
+        help="偏差値を出す際、プロジェクト内の作業者がしきい値未満であれば、偏差値を算出しない。",
+    )
+    parser.add_argument("-o", "--output_dir", required=True, type=Path, help="出力ディレクトリ")
+
+    parser.set_defaults(subcommand_func=main)
+
+
+def main(args):
+    service = build_annofabapi_resource_and_login(args)
+    facade = AnnofabApiFacade(service)
+    WritePerformanceRatingCsv(service, facade, args).main()
+
+
+def add_parser(subparsers: argparse._SubParsersAction):
+    subcommand_name = "write_performance_rating_csv"
+    subcommand_help = "プロジェクトごとユーザごとにパフォーマンスを評価できる複数のCSVを出力します。"
+    description = "プロジェクトごとユーザごとにパフォーマンスを評価できる複数のCSVを出力します。"
+    parser = annofabcli.common.cli.add_parser(
+        subparsers, subcommand_name, subcommand_help, description=description
+    )
+    parse_args(parser)
