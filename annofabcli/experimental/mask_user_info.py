4dce0272b83dd3d9aa9f5f9bd061a85f3e8256e0
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2020-12-24 15:12:49 +0900

    [mask_user_info] biographyが空欄の場合に対応する

--- annofabcli/experimental/mask_user_info.py
+++ annofabcli/experimental/mask_user_info.py
@@ -271,11 +271,14 @@ def replace_biography(
     """
     user_id_column = _get_tuple_column(df, "user_id")
     biography_column = _get_tuple_column(df, "biography")
-
     def _get_biography(row, user_id_column: Any, biography_column: Any) -> str:
         if row[user_id_column] in replacement_dict_by_user_id:
             # マスク対象のユーザなら biographyをマスクする
-            return replacement_dict_by_biography[row[biography_column]]
+            biography = row[biography_column]
+            if biography in replacement_dict_by_biography:
+                return replacement_dict_by_biography[biography]
+            else:
+                return biography
         else:
             return row[biography_column]
 
