f4262d80583aac884de70860634ae8ea573e8f42
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-01-08 20:28:16 +0900

    update test

--- tests/test_experimental.py
+++ tests/test_experimental.py
@@ -3,7 +3,7 @@ import datetime
 import json
 import os
 from pathlib import Path
-
+from annofabcli.__main__ import main
 import annofabapi
 import pandas
 
@@ -30,6 +30,45 @@ annofab_config = dict(inifile.items("annofab"))
 
 project_id = annofab_config["project_id"]
 service = annofabapi.build_from_netrc()
+
+
+class TestCommandLine:
+    command_name = "experimental"
+
+    def test_list_labor_worktime(self):
+        out_file = str(out_dir / "list_labor_worktime.csv")
+        main(
+            [
+                self.command_name,
+                "list_labor_worktime",
+                "--project_id",
+                project_id,
+                "--start_date",
+                "2020-07-01",
+                "--end_date",
+                "2020-07-02",
+                "--output",
+                str(out_file),
+                "--yes",
+            ]
+        )
+
+    def test_dashboad(self):
+        out_file = str(out_dir / "dashboard.csv")
+        main(
+            [
+                self.command_name,
+                "dashboard",
+                "--project_id",
+                project_id,
+                "--date",
+                "2020-07-01",
+                "--output",
+                str(out_file),
+                "--yes",
+            ]
+        )
+
 
 
 class TestListLaborWorktime:
--- tests/test_labor.py
+++ tests/test_labor.py
@@ -6,6 +6,9 @@ import annofabapi
 
 from annofabcli.__main__ import main
 
+out_dir = Path("./tests/out/labor")
+data_dir = Path("./tests/data/labor")
+
 # プロジェクトトップに移動する
 os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/../")
 inifile = configparser.ConfigParser()
@@ -13,20 +16,13 @@ inifile.read("./pytest.ini", "UTF-8")
 annofab_config = dict(inifile.items("annofab"))
 
 project_id = annofab_config["project_id"]
-task_id = annofab_config["task_id"]
-
 service = annofabapi.build_from_netrc()
-user_id = service.api.login_user_id
-
-out_path = Path("./tests/out")
-data_path = Path("./tests/data")
 
 organization_name = service.api.get_organization_of_project(project_id)[0]["organization_name"]
 
-
-class TestLabor:
+class TestCommandLine:
     def test_list_worktime_by_user_with_project_id(self):
-        output_dir = str(out_path / "labor")
+        output_dir = str(out_dir / "labor")
         main(
             [
                 "labor",
@@ -46,7 +42,7 @@ class TestLabor:
         )
 
     def test_list_worktime_by_user_with_organization_name(self):
-        output_dir = str(out_path / "labor")
+        output_dir = str(out_dir / "labor")
         main(
             [
                 "labor",
@@ -61,49 +57,5 @@ class TestLabor:
                 "2019-09-01",
                 "--output_dir",
                 str(output_dir),
-            ]
-        )
-
-
-
-class TestTaskHistory:
-    command_name = "task_history"
-
-
-
-class TestExperimental:
-    command_name = "experimental"
-
-    def test_list_labor_worktime(self):
-        out_file = str(out_path / "list_labor_worktime.csv")
-        main(
-            [
-                self.command_name,
-                "list_labor_worktime",
-                "--project_id",
-                project_id,
-                "--start_date",
-                "2020-07-01",
-                "--end_date",
-                "2020-07-02",
-                "--output",
-                str(out_file),
-                "--yes",
-            ]
-        )
-
-    def test_dashboad(self):
-        out_file = str(out_path / "dashboard.csv")
-        main(
-            [
-                self.command_name,
-                "dashboard",
-                "--project_id",
-                project_id,
-                "--date",
-                "2020-07-01",
-                "--output",
-                str(out_file),
-                "--yes",
             ]
         )
