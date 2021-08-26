6ba13c90c09051a21a7a39b7487a095739a0fad3
Kotet <kotet0418@gmail.com>
2021-08-26 14:46:39 +0900

    デフォルト値の表示が行われるように修正

--- annofabcli/common/cli.py
+++ annofabcli/common/cli.py
@@ -575,7 +575,7 @@ class AbstractCommandLineWithoutWebapiInterface(abc.ABC):
         )
 
 
-class PrettyHelpFormatter(argparse.RawTextHelpFormatter):
+class PrettyHelpFormatter(argparse.RawTextHelpFormatter, argparse.ArgumentDefaultsHelpFormatter):
     def _format_action(self, action: argparse.Action) -> str:
         return super()._format_action(action) + "\n"
 
