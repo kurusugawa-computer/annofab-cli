8fd3ea0f57004e18e32dbe6b1adfc4e3c060a0fe
mercury <w0rkingc4t@gmail.com>
2021-10-02 09:18:50 +0900

    コピー元，コピー先タスクのカプセル化

--- annofabcli/annotation/copy_annotation.py
+++ annofabcli/annotation/copy_annotation.py
@@ -3,13 +3,13 @@ import re
 
 import argparse
 import logging
-from typing import Optional
+from typing import Optional, List 
 
 import annofabapi
 
 import annofabcli
 from annofabcli import AnnofabApiFacade
-from annofabcli.common.cli import AbstractCommandLineInterface, ArgumentParser, build_annofabapi_resource_and_login
+from annofabcli.common.cli import AbstractCommandLineInterface, ArgumentParser, build_annofabapi_resource_and_login, get_list_from_args
 from annofabcli.common.visualize import AddProps
 from annofabapi.wrapper import TaskFrameKey,Wrapper
 
@@ -24,91 +24,106 @@ class CopyAnnotationMain:
 
 
 class CopyAnnotation(AbstractCommandLineInterface):
-    COMMON_MESSAGE = "annofabcli annotation import: error:"
     INPUT_VALIDATE_404_ERROR_MESSAGE = "argument --input: タスクの画像IDを取得しようとしましたが，404エラーが返却されました．指定されたタスクは存在しない可能性があります"
-
-    class INPUT_TYPECHECK_ENUM(Enum):
-        TASK_AND_FILE=auto()
-        TASK_ONLY=auto()
-        FILE_PATH=auto()
-        INVALID=auto()
-
 
     def __init__(self, service: annofabapi.Resource, facade: AnnofabApiFacade, args: argparse.Namespace):
         super().__init__(service, facade, args)
         self.visualize = AddProps(self.service, args.project_id)
-    @classmethod
-    def recognize_input_type(cls, args: argparse.Namespace) -> bool:
-        """
-            引数のバリデーションをする．
-            inputについて，
-            task1:task2
-            task1/input1:task2/input2
-            file:/input.txt
-            のいづれかの形式を保持しているかをチェックする．
-        """ 
-        input_data = args.input
-        if re.match(r'^(\w|-)+\/(\w|-)+:(\w|-)+\/(\w|-)+$',input_data):
-            return cls.INPUT_TYPECHECK_ENUM.TASK_AND_FILE
-        elif re.match(r'^(\w|-)+:(\w|-)+$',input_data):
-            return cls.INPUT_TYPECHECK_ENUM.TASK_ONLY
-        elif re.match(r'file:\/\/(\w|\.)+$',input_data):
-            return cls.INPUT_TYPECHECK_ENUM.FILE_PATH
-        else:
-            return cls.INPUT_TYPECHECK_ENUM.INVALID
+
+    class CopyTasksInfo:
+        COMMON_MESSAGE = "annofabcli annotation import: error:"
+        #片方だけ変更されたりすると困るので，カプセル化しておく
+        __from_task_frame_keys:List[TaskFrameKey]=[]
+        __to_task_frame_keys:List[TaskFrameKey]=[]
+
+        def __init__(self, service: annofabapi.Resource):
+            self.service = service
+        class INPUT_TYPECHECK_ENUM(Enum):
+            TASK_AND_FILE=auto()
+            TASK_ONLY=auto()
+            FILE_PATH=auto()
+            INVALID=auto()
+
+        def append(self,project_id:str, input_data:str, validate_type=None):
+            """
+                input引数，バリデーションタイプから適切にリストを構成する
+            """
+            if validate_type == None:
+                validate_type = self.recognize_input_type(input_data)
+            if validate_type == self.INPUT_TYPECHECK_ENUM.TASK_AND_FILE:
+                # task1/input1:task3:input3の形式
+                from_task_and_input,to_task_and_input = input_data.split(':')
+                from_task,from_input = from_task_and_input.split('/')
+                to_task,to_input = to_task_and_input.split('/')
+                self.__from_task_frame_keys = [TaskFrameKey(project_id=project_id,task_id = from_task,input_data_id=from_input )]
+                self.__to_task_frame_keys = [TaskFrameKey(project_id=project_id,task_id = to_task,input_data_id=to_input )]
+            elif validate_type==self.INPUT_TYPECHECK_ENUM.TASK_ONLY:
+                # task1:task3の形式
+                from_task,to_task = input_data.split(':')
+                #task内に含まれるinput_data_idを全て取得して，__from_task_frame_keysおよび__to_task_frame_keysにappendしていく
+                from_task_or_none = self.service.wrapper.get_task_or_none(project_id=project_id , task_id=from_task)
+                to_task_or_none = self.service.wrapper.get_task_or_none(project_id=project_id , task_id=to_task)
+                if from_task_or_none==None :
+                    logger.error(f"{self.COMMON_MESSAGE} {self.INPUT_VALIDATE_404_ERROR_MESSAGE} ({from_task})")
+                    return
+                elif to_task_or_none==None:
+                    #コピー先のタスクを参照しようとしてエラー
+                    logger.error(f"{self.COMMON_MESSAGE} {self.INPUT_VALIDATE_404_ERROR_MESSAGE} ({to_task})")
+                    return
+                else:
+                    from_input_id_list = from_task_or_none["input_data_id_list"]
+                    to_input_id_list = to_task_or_none["input_data_id_list"]
+                    for from_input,to_input in zip(from_input_id_list, to_input_id_list):
+                        self.__from_task_frame_keys.append(TaskFrameKey(project_id=project_id,task_id = from_task,input_data_id=from_input ))
+                        self.__to_task_frame_keys.append(TaskFrameKey(project_id=project_id,task_id = to_task,input_data_id=to_input ))
+                
+            elif validate_type==self.INPUT_TYPECHECK_ENUM.FILE_PATH:
+                # 以下のように，複数のinput形式が連続するファイルであることが考えられるので，全てに対応する
+                for line in get_list_from_args(input_data):
+                    self.append(line)
+
+            elif validate_type==self.INPUT_TYPECHECK_ENUM.INVALID:
+                logger.error(f"{self.COMMON_MESSAGE} argument --input: 入力されたタスクを正しく解釈できませんでした({input_data})")
+                return 
+            else:
+                #想定外
+                pass
+
+        def get_tasks(self):
+            return zip(self.__from_task_frame_keys,self.__to_task_frame_keys)
+
+
+        @classmethod
+        def recognize_input_type(cls, input_data: str) -> bool:
+            """
+                引数のバリデーションをする．
+                inputについて，
+                task1:task2
+                task1/input1:task2/input2
+                file:/input.txt
+                のいづれかの形式を保持しているかをチェックする．
+            """ 
+            if type(get_list_from_args([input_data]))=='list' and input_data:
+                return cls.INPUT_TYPECHECK_ENUM.FILE_PATH
+            if re.match(r'^(\w|-)+\/(\w|-)+:(\w|-)+\/(\w|-)+$',input_data):
+                return cls.INPUT_TYPECHECK_ENUM.TASK_AND_FILE
+            elif re.match(r'^(\w|-)+:(\w|-)+$',input_data):
+                return cls.INPUT_TYPECHECK_ENUM.TASK_ONLY
+            else:
+                return cls.INPUT_TYPECHECK_ENUM.INVALID
 
 
 
     def main(self):
         args = self.args
-        validate_result = CopyAnnotation.recognize_input_type(args)
-        logger.debug(validate_result)
-        
         project_id = args.project_id
         input_data = args.input
 
-        from_frame_keys:list[TaskFrameKey]=[]
-        to_frame_keys:list[TaskFrameKey]=[]
+        copy_tasks_info = self.CopyTasksInfo(self.service)
 
-        if validate_result == self.INPUT_TYPECHECK_ENUM.TASK_AND_FILE:
-            # task1/input1:task3:input3の形式
-            from_task_and_input,to_task_and_input = input_data.split(':')
-            from_task,from_input = from_task_and_input.split('/')
-            to_task,to_input = to_task_and_input.split('/')
-            from_frame_keys = [TaskFrameKey(project_id=project_id,task_id = from_task,input_data_id=from_input )]
-            to_frame_keys = [TaskFrameKey(project_id=project_id,task_id = to_task,input_data_id=to_input )]
-        elif validate_result==self.INPUT_TYPECHECK_ENUM.TASK_ONLY:
-            # task1:task3の形式
-            from_task,to_task = input_data.split(':')
-            #task内に含まれるinput_data_idを全て取得して，from_frame_keysおよびto_frame_keysにappendしていく
-            from_task_or_none = self.service.wrapper.get_task_or_none(project_id=project_id , task_id=from_task)
-            to_task_or_none = self.service.wrapper.get_task_or_none(project_id=project_id , task_id=to_task)
-            if from_task_or_none==None :
-                logger.error(f"{self.COMMON_MESSAGE} {self.INPUT_VALIDATE_404_ERROR_MESSAGE} ({from_task})")
-                return
-            elif to_task_or_none==None:
-                #コピー先のタスクを参照しようとしてエラー
-                logger.error(f"{self.COMMON_MESSAGE} {self.INPUT_VALIDATE_404_ERROR_MESSAGE} ({to_task})")
-                return
-            else:
-                from_input_id_list = from_task_or_none["input_data_id_list"]
-                to_input_id_list = to_task_or_none["input_data_id_list"]
-                for from_input,to_input in zip(from_input_id_list, to_input_id_list):
-                    from_frame_keys.append(TaskFrameKey(project_id=project_id,task_id = from_task,input_data_id=from_input ))
-                    to_frame_keys.append(TaskFrameKey(project_id=project_id,task_id = to_task,input_data_id=to_input ))
-
-        elif validate_result==self.INPUT_TYPECHECK_ENUM.FILE_PATH:
-            # 以下のように，複数のinput形式が連続するファイルであることが考えられるので，全てに対応する
-            # task1:task3
-            # task2/input2:task4/input4
-            pass
-        elif validate_result==self.INPUT_TYPECHECK_ENUM.INVALID:
-            logger.error(f"{self.COMMON_MESSAGE} argument --input: 入力されたタスクを正しく解釈できませんでした( {input_data} )")
-            return 
-        else:
-            #想定外
-            pass
-        for from_frame_key,to_frame_key in zip(from_frame_keys,to_frame_keys):
+        copy_tasks_info.append(project_id, input_data)
+        
+        for from_frame_key,to_frame_key in copy_tasks_info.get_tasks():
             try:
                 self.service.wrapper.copy_annotation(src=from_frame_key,dest=to_frame_key, annotation_specs_relation=None)
             except Exception as e:
