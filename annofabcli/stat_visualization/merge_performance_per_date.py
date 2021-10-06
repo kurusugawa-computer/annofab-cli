b9f96006ddb1e4999e2076af9997f92731189356
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-10-06 14:00:40 +0900

    typo

--- annofabcli/stat_visualization/merge_performance_per_date.py
+++ annofabcli/stat_visualization/merge_performance_per_date.py
@@ -13,7 +13,7 @@ from annofabcli.statistics.table import Table
 logger = logging.getLogger(__name__)
 
 
-def create_df_merged_peformance_per_date(csv_path_list: List[Path]) -> pandas.DataFrame:
+def create_df_merged_performance_per_date(csv_path_list: List[Path]) -> pandas.DataFrame:
     """
     `日毎の生産量と生産性.csv` をマージしたDataFrameを返す。
 
@@ -32,6 +32,10 @@ def create_df_merged_peformance_per_date(csv_path_list: List[Path]) -> pandas.Da
             logger.warning(f"{csv_path} は存在しませんでした。")
             continue
 
+    if len(df_list) == 0:
+        logger.warning(f"マージ対象のCSVファイルは存在しませんでした。")
+        return pandas.DataFrame()
+
     sum_df = df_list[0]
     for df in df_list[1:]:
         sum_df = Table.merge_whole_productivity_per_date(sum_df, df)
@@ -39,20 +43,24 @@ def create_df_merged_peformance_per_date(csv_path_list: List[Path]) -> pandas.Da
     return sum_df
 
 
-def merge_peformance_per_date(csv_path_list: List[Path], output_path: Path) -> None:
-    sum_df = create_df_merged_peformance_per_date(csv_path_list)
+def merge_performance_per_date(csv_path_list: List[Path], output_path: Path) -> None:
+    sum_df = create_df_merged_performance_per_date(csv_path_list)
+    if len(sum_df) == 0:
+        logger.warning(f"出力対象のデータが0件であるため、CSVファイルを出力しません。")
+        return
+
     csv_obj = Csv(outdir=str(output_path.parent))
     csv_obj.write_whole_productivity_per_date(sum_df, output_path=output_path)
 
 
-class MergePerfomancePerDate(AbstractCommandLineWithoutWebapiInterface):
+class MergePerformancePerDate(AbstractCommandLineWithoutWebapiInterface):
     def main(self):
         args = self.args
-        merge_peformance_per_date(csv_path_list=args.csv, output_path=args.output)
+        merge_performance_per_date(csv_path_list=args.csv, output_path=args.output)
 
 
 def main(args):
-    MergePerfomancePerDate(args).main()
+    MergePerformancePerDate(args).main()
 
 
 def parse_args(parser: argparse.ArgumentParser):
@@ -70,7 +78,7 @@ def parse_args(parser: argparse.ArgumentParser):
 
 
 def add_parser(subparsers: Optional[argparse._SubParsersAction] = None):
-    subcommand_name = "merge_peformance_csv_per_date"
+    subcommand_name = "merge_performance_csv_per_date"
     subcommand_help = f"``annofabcli statistics visualize`` コマンドの出力ファイル'{FILENAME_PEFORMANCE_PER_DATE}'をマージします。"
     description = f"``annofabcli statistics visualize`` コマンドの出力ファイル'{FILENAME_PEFORMANCE_PER_DATE}'をマージします。"
     parser = annofabcli.common.cli.add_parser(subparsers, subcommand_name, subcommand_help, description)
