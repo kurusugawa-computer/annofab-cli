f8d3891e502c34450bfcb6124e270272c75419c3
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-12-08 14:39:21 +0900

    update metadat

--- annofabcli/task/update_metadata_of_task.py
+++ annofabcli/task/update_metadata_of_task.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+import sys
 import argparse
 import logging
 import multiprocessing
@@ -13,6 +13,7 @@ import annofabcli
 import annofabcli.common.cli
 from annofabcli import AnnofabApiFacade
 from annofabcli.common.cli import (
+    COMMAND_LINE_ERROR_STATUS_CODE,
     AbstractCommandLineInterface,
     AbstractCommandLineWithConfirmInterface,
     ArgumentParser,
@@ -30,6 +31,12 @@ class UpdateMetadataOfTaskMain(AbstractCommandLineWithConfirmInterface):
         self.is_overwrite_metadata = is_overwrite_metadata
         AbstractCommandLineWithConfirmInterface.__init__(self, all_yes)
 
+    def get_confirm_message(self, task_id: str, metadata: dict[str, Any]) -> str:
+        if self.is_overwrite_metadata:
+            return f"タスク '{task_id}' のメタデータを '{metadata}' に変更しますか？ "
+        else:
+            return f"タスク '{task_id}' のメタデータに '{metadata}' を追加しますか？ "
+
     def set_metadata_to_task(
         self,
         project_id: str,
@@ -44,7 +51,9 @@ class UpdateMetadataOfTaskMain(AbstractCommandLineWithConfirmInterface):
             logger.warning(f"{logging_prefix} タスク '{task_id}' は存在しないのでスキップします。")
             return False
 
-        if not self.confirm_processing(f"タスク '{task_id}' のメタデータを更新しますか？ "):
+        logger.debug(f"task_id: {task_id}, metadata='{task['metadata']}'")
+
+        if not self.confirm_processing(self.get_confirm_message(task_id, metadata)):
             return False
 
         task["last_updated_datetime"] = task["updated_datetime"]
@@ -66,7 +75,7 @@ class UpdateMetadataOfTaskMain(AbstractCommandLineWithConfirmInterface):
             task_index=task_index,
         )
 
-    def update_metadata_of_task2(
+    def update_metadata_of_task(
         self,
         project_id: str,
         task_ids: Collection[str],
@@ -74,9 +83,9 @@ class UpdateMetadataOfTaskMain(AbstractCommandLineWithConfirmInterface):
         parallelism: Optional[int] = None,
     ):
         if self.is_overwrite_metadata:
-            logger.info(f"{len(task_ids)} 件のタスクのmetadataを、{metadata} に変更します（上書き）。")
+            logger.info(f"{len(task_ids)} 件のタスクのメタデータを、{metadata} に変更します（上書き）。")
         else:
-            logger.info(f"{len(task_ids)} 件のタスクのmetadataに、{metadata} を追加します。")
+            logger.info(f"{len(task_ids)} 件のタスクのメタデータに、{metadata} を追加します。")
 
         if self.is_overwrite_metadata and self.all_yes:
             self.update_metadata_with_patch_tasks_metadata_api(project_id, task_ids=task_ids, metadata=metadata)
@@ -95,7 +104,7 @@ class UpdateMetadataOfTaskMain(AbstractCommandLineWithConfirmInterface):
             else:
                 # 逐次処理
                 for task_index, task_id in enumerate(task_ids):
-                    result = self.set_metadata_to_task_wrapper(
+                    result = self.set_metadata_to_task(
                         project_id,
                         task_id,
                         metadata=metadata,
@@ -115,21 +124,41 @@ class UpdateMetadataOfTaskMain(AbstractCommandLineWithConfirmInterface):
         BATCH_SIZE = 500
         logger.info(f"{len(task_ids)} 件のタスクのmetadataを{metadata} に、{BATCH_SIZE}個ずつ変更します。")
         first_index = 0
-        while first_index < len(task_ids):
-            logger.info(f"{first_index+1} 〜 {min(first_index+BATCH_SIZE, len(task_ids))} 件目のタスクのmetadataを更新します。")
-            request_body = {task_id: metadata for task_id in task_ids[first_index : first_index + BATCH_SIZE]}
+        task_id_list = list(task_ids)
+        while first_index < len(task_id_list):
+            logger.info(f"{first_index+1} 〜 {min(first_index+BATCH_SIZE, len(task_id_list))} 件目のタスクのmetadataを更新します。")
+            request_body = {task_id: metadata for task_id in task_id_list[first_index : first_index + BATCH_SIZE]}
             self.service.api.patch_tasks_metadata(project_id, request_body=request_body)
             first_index += BATCH_SIZE
 
 
 class UpdateMetadataOfTask(AbstractCommandLineInterface):
+    @staticmethod
+    def validate(args: argparse.Namespace) -> bool:
+        COMMON_MESSAGE = "annofabcli task update_metadata: error:"
+
+        if args.parallelism is not None and not args.yes:
+            print(
+                f"{COMMON_MESSAGE} argument --parallelism: '--parallelism' を指定するときは、必ず '--yes' を指定してください。",
+                file=sys.stderr,
+            )
+            return False
+
+        return True
+
     def main(self):
         args = self.args
+
+        if not self.validate(args):
+            sys.exit(COMMAND_LINE_ERROR_STATUS_CODE)
+
         task_id_list = annofabcli.common.cli.get_list_from_args(args.task_id)
         metadata = annofabcli.common.cli.get_json_from_args(args.metadata)
         super().validate_project(args.project_id, [ProjectMemberRole.OWNER, ProjectMemberRole.TRAINING_DATA_USER])
         main_obj = UpdateMetadataOfTaskMain(self.service, is_overwrite_metadata=args.overwrite, all_yes=args.yes)
-        main_obj.update_metadata_of_task(args.project_id, task_id_list=task_id_list, metadata=metadata)
+        main_obj.update_metadata_of_task(
+            args.project_id, task_ids=task_id_list, metadata=metadata, parallelism=args.parallelism
+        )
 
 
 def main(args):
@@ -160,7 +189,6 @@ def parse_args(parser: argparse.ArgumentParser):
     parser.add_argument(
         "--parallelism", type=int, help="使用するプロセス数（並列度）を指定してください。指定する場合は必ず ``--yes`` を指定してください。指定しない場合は、逐次的に処理します。"
     )
-
 
     parser.set_defaults(subcommand_func=main)
 
