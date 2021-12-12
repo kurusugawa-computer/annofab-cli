9691d1a86185e934e0131e811335b68abda5cf43
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-12-12 11:04:23 +0900

    yes

--- annofabcli/statistics/visualization/dataframe/productivity_per_date.py
+++ annofabcli/statistics/visualization/dataframe/productivity_per_date.py
@@ -405,3 +405,381 @@ class AnnotatorProductivityPerDate:
         )
 
         print_csv(df[columns], output=str(output_file))
+
+
+
+class AcceptorProductivityPerDate:
+    """受入開始日ごとの受入者の生産性に関する情報"""
+
+    @classmethod
+    def create(cls, df_task: pandas.DataFrame) -> pandas.DataFrame:
+        """
+        日毎、ユーザごとの情報を出力する。
+
+        Args:
+            task_df:
+
+        Returns:
+
+        """
+        new_df = df_task.copy()
+        new_df["first_annotation_started_date"] = new_df["first_annotation_started_datetime"].map(
+            lambda e: datetime_to_date(e) if e is not None and isinstance(e, str) else None
+        )
+        new_df["task_count"] = 1  # 集計用
+
+        # first_annotation_user_id と first_annotation_usernameの両方を指定している理由：
+        # first_annotation_username を取得するため
+        group_obj = new_df.groupby(
+            ["first_annotation_started_date", "first_annotation_user_id", "first_annotation_username"],
+            as_index=False,
+        )
+
+        sum_df = group_obj[
+            [
+                "first_annotation_worktime_hour",
+                "annotation_worktime_hour",
+                "inspection_worktime_hour",
+                "acceptance_worktime_hour",
+                "sum_worktime_hour",
+                "task_count",
+                "input_data_count",
+                "annotation_count",
+                "inspection_count",
+            ]
+        ].sum()
+
+        for denominator_column in ["input_data_count", "annotation_count"]:
+            for phase in ["annotation", "inspection", "acceptance"]:
+                numerator_column = f"{phase}_worktime_hour"
+                sum_df[f"{numerator_column}/{denominator_column}"] = (
+                    sum_df[numerator_column] / sum_df[denominator_column]
+                )
+
+        sum_df["inspection_count/annotation_count"] = sum_df["inspection_count"] / sum_df["annotation_count"]
+        sum_df["inspection_count/input_data_count"] = sum_df["inspection_count"] / sum_df["annotation_count"]
+
+        return sum_df
+
+    @classmethod
+    def plot_annotation_metrics(
+        cls,
+        df: pandas.DataFrame,
+        output_file: Path,
+        target_user_id_list: Optional[list[str]] = None,
+    ):
+        """
+        生産性を教師付作業者ごとにプロットする。
+
+        Args:
+            df:
+            first_annotation_user_id_list:
+
+        Returns:
+
+        """
+
+        tooltip_item = [
+            "first_annotation_user_id",
+            "first_annotation_username",
+            "first_annotation_started_date",
+            "annotation_worktime_hour",
+            "task_count",
+            "input_data_count",
+            "annotation_count",
+            "inspection_count",
+        ]
+
+        if len(df) == 0:
+            logger.info("データが0件のため出力しない")
+            return
+
+        df = df.copy()
+
+        df["annotation_worktime_minute/annotation_count"] = df["annotation_worktime_hour"] * 60 / df["annotation_count"]
+
+        if target_user_id_list is not None:
+            user_id_list = target_user_id_list
+        else:
+            user_id_list = (
+                df.sort_values(by="first_annotation_started_date", ascending=False)["first_annotation_user_id"]
+                .dropna()
+                .unique()
+                .tolist()
+            )
+
+        user_id_list = get_plotted_user_id_list(user_id_list)
+
+        df["dt_first_annotation_started_date"] = df["first_annotation_started_date"].map(lambda e: parse(e).date())
+
+        fig_info_list = [
+            dict(
+                title="教師付開始日ごとの教師付作業時間",
+                y_column_name="annotation_worktime_hour",
+                y_axis_label="教師付作業時間[hour]",
+            ),
+            dict(
+                title="教師付開始日ごとのアノテーションあたり教師付作業時間",
+                y_column_name="annotation_worktime_minute/annotation_count",
+                y_axis_label="アノテーションあたり教師付時間[min/annotation]",
+            ),
+            dict(
+                title="教師付開始日ごとのアノテーションあたり検査コメント数",
+                y_column_name="inspection_count/annotation_count",
+                y_axis_label="アノテーションあたり検査コメント数",
+            ),
+            dict(
+                title="教師付開始日ごとのアノテーションあたり教師付作業時間(1週間移動平均)",
+                y_column_name=f"annotation_worktime_minute/annotation_count{WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX}",
+                y_axis_label="アノテーションあたり教師付時間[min/annotation]",
+            ),
+            dict(
+                title="教師付開始日ごとのアノテーションあたり検査コメント数(1週間移動平均)",
+                y_column_name=f"inspection_count/annotation_count{WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX}",
+                y_axis_label="アノテーションあたり検査コメント数",
+            ),
+        ]
+
+        logger.debug(f"{output_file} を出力します。")
+
+        figs: list[bokeh.plotting.Figure] = []
+        for fig_info in fig_info_list:
+            figs.append(
+                figure(
+                    plot_width=1200,
+                    plot_height=600,
+                    title=fig_info["title"],
+                    x_axis_label="教師付開始日",
+                    x_axis_type="datetime",
+                    y_axis_label=fig_info["y_axis_label"],
+                )
+            )
+
+        for user_index, user_id in enumerate(user_id_list):
+            df_subset = df[df["first_annotation_user_id"] == user_id]
+            if df_subset.empty:
+                logger.debug(f"dataframe is empty. user_id = {user_id}")
+                continue
+
+            df_subset[f"annotation_worktime_minute/annotation_count{WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX}"] = (
+                get_weekly_moving_average(df_subset["annotation_worktime_hour"])
+                * 60
+                / get_weekly_moving_average(df_subset["annotation_count"])
+            )
+            df_subset[
+                f"inspection_count/annotation_count{WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX}"
+            ] = get_weekly_moving_average(df_subset["inspection_count"]) / get_weekly_moving_average(
+                df_subset["annotation_count"]
+            )
+
+            source = ColumnDataSource(data=df_subset)
+            color = get_color_from_small_palette(user_index)
+            username = df_subset.iloc[0]["first_annotation_username"]
+
+            for fig, fig_info in zip(figs, fig_info_list):
+                y_column_name = fig_info["y_column_name"]
+                if y_column_name.endswith(WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX):
+                    plot_moving_average(
+                        fig,
+                        source=source,
+                        x_column_name="dt_first_annotation_started_date",
+                        y_column_name=y_column_name,
+                        legend_label=username,
+                        color=color,
+                    )
+
+                else:
+                    plot_line_and_circle(
+                        fig,
+                        x_column_name="dt_first_annotation_started_date",
+                        y_column_name=y_column_name,
+                        source=source,
+                        legend_label=username,
+                        color=color,
+                    )
+
+        hover_tool = create_hover_tool(tooltip_item)
+        for fig in figs:
+            fig.add_tools(hover_tool)
+            add_legend_to_figure(fig)
+
+        bokeh.plotting.reset_output()
+        bokeh.plotting.output_file(output_file, title=output_file.stem)
+        bokeh.plotting.save(bokeh.layouts.column(figs))
+
+    @classmethod
+    def plot_input_data_metrics(
+        cls,
+        df: pandas.DataFrame,
+        output_file: Path,
+        target_user_id_list: Optional[list[str]] = None,
+    ):
+        """
+        教師付者の入力データ単位の生産性情報を折れ線グラフでプロットする。
+        """
+
+        tooltip_item = [
+            "first_annotation_user_id",
+            "first_annotation_username",
+            "first_annotation_started_date",
+            "annotation_worktime_hour",
+            "task_count",
+            "input_data_count",
+            "annotation_count",
+            "inspection_count",
+        ]
+
+        if len(df) == 0:
+            logger.info("データが0件のため出力しない")
+            return
+
+        df = df.copy()
+
+        df["annotation_worktime_minute/input_data_count"] = df["annotation_worktime_hour"] * 60 / df["input_data_count"]
+
+        if target_user_id_list is not None:
+            user_id_list = target_user_id_list
+        else:
+            user_id_list = (
+                df.sort_values(by="first_annotation_started_date", ascending=False)["first_annotation_user_id"]
+                .dropna()
+                .unique()
+                .tolist()
+            )
+
+        user_id_list = get_plotted_user_id_list(user_id_list)
+
+        df["dt_first_annotation_started_date"] = df["first_annotation_started_date"].map(lambda e: parse(e).date())
+
+        fig_info_list = [
+            dict(
+                title="教師付開始日ごとの教師付作業時間",
+                y_column_name="annotation_worktime_hour",
+                y_axis_label="教師付作業時間[hour]",
+            ),
+            dict(
+                title="教師付開始日ごとの入力データあたり教師付作業時間",
+                y_column_name="annotation_worktime_minute/input_data_count",
+                y_axis_label="入力データあたり教師付時間[min/input_data]",
+            ),
+            dict(
+                title="教師付開始日ごとの入力データあたり検査コメント数",
+                y_column_name="inspection_count/input_data_count",
+                y_axis_label="入力データあたり検査コメント数",
+            ),
+            dict(
+                title="教師付開始日ごとの入力データあたり教師付作業時間(1週間移動平均)",
+                y_column_name=f"annotation_worktime_minute/input_data_count{WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX}",
+                y_axis_label="入力データあたり教師付時間[min/annotation]",
+            ),
+            dict(
+                title="教師付開始日ごとの入力データあたり検査コメント数(1週間移動平均)",
+                y_column_name=f"inspection_count/input_data_count{WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX}",
+                y_axis_label="入力データあたり検査コメント数",
+            ),
+        ]
+
+        logger.debug(f"{output_file} を出力します。")
+
+        figs: list[bokeh.plotting.Figure] = []
+        for fig_info in fig_info_list:
+            figs.append(
+                figure(
+                    plot_width=1200,
+                    plot_height=600,
+                    title=fig_info["title"],
+                    x_axis_label="教師付開始日",
+                    x_axis_type="datetime",
+                    y_axis_label=fig_info["y_axis_label"],
+                )
+            )
+
+        for user_index, user_id in enumerate(user_id_list):
+            df_subset = df[df["first_annotation_user_id"] == user_id]
+            if df_subset.empty:
+                logger.debug(f"dataframe is empty. user_id = {user_id}")
+                continue
+
+            df_subset[f"annotation_worktime_minute/input_data_count{WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX}"] = (
+                get_weekly_moving_average(df_subset["annotation_worktime_hour"])
+                * 60
+                / get_weekly_moving_average(df_subset["input_data_count"])
+            )
+            df_subset[
+                f"inspection_count/annotation_count{WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX}"
+            ] = get_weekly_moving_average(df_subset["inspection_count"]) / get_weekly_moving_average(
+                df_subset["input_data_count"]
+            )
+
+            source = ColumnDataSource(data=df_subset)
+            color = get_color_from_small_palette(user_index)
+            username = df_subset.iloc[0]["first_annotation_username"]
+
+            for fig, fig_info in zip(figs, fig_info_list):
+                y_column_name = fig_info["y_column_name"]
+                if y_column_name.endswith(WEEKLY_MOVING_AVERAGE_COLUMN_SUFFIX):
+                    plot_moving_average(
+                        fig,
+                        source=source,
+                        x_column_name="dt_first_annotation_started_date",
+                        y_column_name=y_column_name,
+                        legend_label=username,
+                        color=color,
+                    )
+
+                else:
+                    plot_line_and_circle(
+                        fig,
+                        x_column_name="dt_first_annotation_started_date",
+                        y_column_name=y_column_name,
+                        source=source,
+                        legend_label=username,
+                        color=color,
+                    )
+
+        hover_tool = create_hover_tool(tooltip_item)
+        for fig in figs:
+            fig.add_tools(hover_tool)
+            add_legend_to_figure(fig)
+
+        bokeh.plotting.reset_output()
+        bokeh.plotting.output_file(output_file, title=output_file.stem)
+        bokeh.plotting.save(bokeh.layouts.column(figs))
+
+    @classmethod
+    def to_csv(cls, df: pandas.DataFrame, output_file: Path) -> None:
+        """
+        日毎の全体の生産量、生産性を出力する。
+
+        """
+        if len(df) == 0:
+            logger.warning(f"データ件数が0件のため {output_file} は出力しません。")
+            return
+
+        production_columns = [
+            "first_annotation_started_date",
+            "first_annotation_user_id",
+            "first_annotation_username",
+            "task_count",
+            "input_data_count",
+            "annotation_count",
+            "inspection_count",
+            "sum_worktime_hour",
+            "annotation_worktime_hour",
+            "inspection_worktime_hour",
+            "acceptance_worktime_hour",
+        ]
+
+        velocity_columns = [
+            f"{numerator}/{denominator}"
+            for numerator in ["annotation_worktime_hour", "inspection_worktime_hour", "acceptance_worktime_hour"]
+            for denominator in ["input_data_count", "input_data_count"]
+        ]
+
+        columns = (
+            production_columns
+            + velocity_columns
+            + ["inspection_count/input_data_count", "inspection_count/annotation_count"]
+        )
+
+        print_csv(df[columns], output=str(output_file))
