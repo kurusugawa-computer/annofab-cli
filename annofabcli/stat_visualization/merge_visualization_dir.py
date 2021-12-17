6fb5a0fb8e865ed059bc04a2e3270efd82cc9e88
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2021-12-17 17:07:29 +0900

    update

--- annofabcli/stat_visualization/merge_visualization_dir.py
+++ annofabcli/stat_visualization/merge_visualization_dir.py
@@ -125,7 +125,7 @@ def merge_visualization_dir(  # pylint: disable=too-many-statements
         user_id_list=user_id_list,
     )
     write_task_histogram(
-        csv=output_dir / FILENAME_TASK_LIST, output_dir=output_dir / "histogram", minimal_output=minimal_output
+        csv=output_dir / FILENAME_TASK_LIST, output_dir=output_dir / "histogram"
     )
 
     # info.jsonを出力
