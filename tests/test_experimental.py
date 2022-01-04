fa2808d5da3c3e8f39fa5839a08e8040bd30e348
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-01-04 18:14:04 +0900

    test 修正

--- tests/test_experimental.py
+++ tests/test_experimental.py
@@ -1,18 +1,10 @@
 import configparser
-import datetime
-import json
 import os
 from pathlib import Path
 
 import annofabapi
 
 from annofabcli.__main__ import main
-from annofabcli.experimental.dashboard import (
-    PrintDashBoardMain,
-    RemainingTaskCount,
-    get_remaining_task_count_info_from_task_list,
-    get_worktime_for_period,
-)
 
 # プロジェクトトップに移動する
 os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/../")
@@ -82,37 +74,3 @@ class TestCommandLine:
                 "--yes",
             ]
         )
-
-
-class TestDashboard:
-    @classmethod
-    def setup_class(cls):
-        cls.main_obj = PrintDashBoardMain(service)
-
-    task_list = [
-        {"status_for_summary": "complete", "updated_datetime": "2020-04-01T18:36:11.159+09:00"},
-        {"status_for_summary": "annotation_not_started", "updated_datetime": "2020-04-02T18:36:11.159+09:00"},
-    ]
-
-    def test_get_remaining_task_count_info_from_task_list(self):
-        actual = get_remaining_task_count_info_from_task_list(self.task_list)
-        assert actual == RemainingTaskCount(complete=1, annotation_not_started=1)
-
-    def test_get_worktime_for_period(self):
-        actual_worktime_dict = {
-            "2020-04-01": 1.0,
-            "2020-04-02": 10.5,
-            "2020-04-04": 5.5,
-        }
-        actual = get_worktime_for_period(
-            actual_worktime_dict, lower_date=datetime.date(2020, 4, 2), upper_date=datetime.date(2020, 4, 4)
-        )
-        assert actual == 16.0
-
-    def test_get_task_phase_statistics(self):
-        actual = self.main_obj.get_task_phase_statistics(project_id)
-
-    def test_create_dashboard_values(self):
-        with open(test_dir / "task.json") as f:
-            task_list = json.load(f)
-        actual = self.main_obj.create_dashboard_data(project_id, date="2020-07-01", task_list=task_list)
