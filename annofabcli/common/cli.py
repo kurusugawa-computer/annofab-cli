c78463b07ab966f2ec81d45745d8aec120e45fdb
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-01-28 00:00:31 +0900

    herupuメッセージの"--help"を"global optional arguments"に移動した

--- annofabcli/common/cli.py
+++ annofabcli/common/cli.py
@@ -20,6 +20,7 @@ import yaml
 from annofabapi.api import DEFAULT_ENDPOINT_URL
 from annofabapi.exceptions import AnnofabApiException
 from annofabapi.models import OrganizationMemberRole, ProjectMemberRole
+from more_itertools import first_true
 
 import annofabcli
 from annofabcli.common.dataclasses import WaitOptions
@@ -83,6 +84,33 @@ def add_parser(
         サブコマンドのparser
 
     """
+    GLOBAL_OPTIONAL_ARGUMENTS_TITLE = "global optional arguments"
+
+    def create_parent_parser() -> argparse.ArgumentParser:
+        """
+        共通の引数セットを生成する。
+        """
+        parent_parser = argparse.ArgumentParser(add_help=False)
+        group = parent_parser.add_argument_group(GLOBAL_OPTIONAL_ARGUMENTS_TITLE)
+
+        group.add_argument("--yes", action="store_true", help="処理中に現れる問い合わせに対して、常に ``yes`` と回答します。")
+
+        group.add_argument(
+            "--endpoint_url", type=str, help=f"AnnoFab WebAPIのエンドポイントを指定します。指定しない場合は ``{DEFAULT_ENDPOINT_URL}`` です。"
+        )
+
+        group.add_argument(
+            "--logdir",
+            type=Path,
+            default=".log",
+            help="ログファイルを保存するディレクトリを指定します。指定しない場合は ``.log`` ディレクトリ'にログファイルが保存されます。",
+        )
+
+        group.add_argument("--disable_log", action="store_true", help="ログを無効にします。")
+
+        group.add_argument("--debug", action="store_true", help="HTTPリクエストの内容やレスポンスのステータスコードなど、デバッグ用のログが出力されます。")
+
+        return parent_parser
 
     if subparsers is None:
         subparsers = argparse.ArgumentParser().add_subparsers()
@@ -97,31 +125,19 @@ def add_parser(
         formatter_class=PrettyHelpFormatter,
     )
     parser.set_defaults(command_help=parser.print_help)
+
+    # 引数グループに"global optional group"がある場合は、"--help"オプションをデフォルトの"optional"グループから、"global optional arguments"グループに移動する
+    # https://ja.stackoverflow.com/a/57313/19524
+    global_optional_argument_group = first_true(
+        parser._action_groups, pred=lambda e: e.title == GLOBAL_OPTIONAL_ARGUMENTS_TITLE
+    )
+    if global_optional_argument_group is not None:
+        # optional グループの 0番目が help なので取り出す
+        help_action = parser._optionals._group_actions.pop(0)
+        assert help_action.dest == "help"
+        # global optional group の 先頭にhelpを追加
+        global_optional_argument_group._group_actions.insert(0, help_action)
     return parser
-
-
-def create_parent_parser() -> argparse.ArgumentParser:
-    """
-    共通の引数セットを生成する。
-    """
-    parent_parser = argparse.ArgumentParser(add_help=False)
-    group = parent_parser.add_argument_group("global optional arguments")
-
-    group.add_argument("--yes", action="store_true", help="処理中に現れる問い合わせに対して、常に ``yes`` と回答します。")
-
-    group.add_argument(
-        "--endpoint_url", type=str, help=f"AnnoFab WebAPIのエンドポイントを指定します。指定しない場合は ``{DEFAULT_ENDPOINT_URL}`` です。"
-    )
-
-    group.add_argument(
-        "--logdir", type=Path, default=".log", help="ログファイルを保存するディレクトリを指定します。指定しない場合は ``.log`` ディレクトリ'にログファイルが保存されます。"
-    )
-
-    group.add_argument("--disable_log", action="store_true", help="ログを無効にします。")
-
-    group.add_argument("--debug", action="store_true", help="HTTPリクエストの内容やレスポンスのステータスコードなど、デバッグ用のログが出力されます。")
-
-    return parent_parser
 
 
 def get_list_from_args(str_list: Optional[List[str]] = None) -> List[str]:
