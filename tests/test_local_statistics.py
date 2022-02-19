47cf7fd9035984009fee3a622f8b145e20f380f8
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-02-19 23:03:54 +0900

    update test

--- tests/test_local_statistics.py
+++ tests/test_local_statistics.py
@@ -459,23 +459,23 @@ class TestListAnnotationCounterByInputData:
         counter = ListAnnotationCounterByInputData.get_annotation_counter(annotation)
         assert counter.input_data_id == "input1"
         assert counter.task_id == "task1"
-        assert counter.labels_counter == collections.Counter({"bird": 2, "climatic": 1})
-        assert counter.attributes_counter == collections.Counter(
+        assert counter.annotation_count_by_label == collections.Counter({"bird": 2, "climatic": 1})
+        assert counter.annotation_count_by_attribute == collections.Counter(
             {
                 ("bird", "weight", "4"): 1,
                 ("bird", "weight", "3"): 1,
-                ("bird", "occluded", "True"): 2,
+                ("bird", "occluded", "true"): 2,
                 ("climatic", "weather", "sunny"): 1,
             }
         )
 
         counter2 = ListAnnotationCounterByInputData.get_annotation_counter(
-            annotation, target_labels=["climatic"], target_attributes=[("bird", "occluded", "True")]
+            annotation, target_labels=["climatic"], target_attributes=[("bird", "occluded", "true")]
         )
-        assert counter2.labels_counter == collections.Counter({"climatic": 1})
-        assert counter2.attributes_counter == collections.Counter(
+        assert counter2.annotation_count_by_label == collections.Counter({"climatic": 1})
+        assert counter2.annotation_count_by_attribute == collections.Counter(
             {
-                ("bird", "occluded", "True"): 2,
+                ("bird", "occluded", "true"): 2,
             }
         )
 
@@ -489,7 +489,7 @@ class TestListAnnotationCounterByInputData:
         counter_list = ListAnnotationCounterByInputData.get_annotation_counter_list(
             data_path / "simple-annotations.zip"
         )
-        ListAnnotationCounterByInputData.print_labels_count(
+        ListAnnotationCounterByInputData.print_labels_count_csv(
             counter_list,
             output_file=out_path / "list_annotation_count/labels_count_by_input_data.csv",
             label_columns=["dog", "human"],
@@ -499,7 +499,7 @@ class TestListAnnotationCounterByInputData:
         counter_list = ListAnnotationCounterByInputData.get_annotation_counter_list(
             data_path / "simple-annotations.zip"
         )
-        ListAnnotationCounterByInputData.print_attributes_count(
+        ListAnnotationCounterByInputData.print_attributes_count_csv(
             counter_list,
             output_file=out_path / "list_annotation_count/attributes_count_by_input_data.csv",
             attribute_columns=[("Cat", "occluded", "True"), ("climatic", "temparature", "20")],
@@ -511,20 +511,20 @@ class TestListAnnotationCounterByTask:
         counter_list = ListAnnotationCounterByTask.get_annotation_counter_list(data_path / "simple-annotations.zip")
         assert len(counter_list) == 2
 
-    def test_print_labels_count(self):
+    def test_print_labels_count_csv(self):
         counter_list = ListAnnotationCounterByTask.get_annotation_counter_list(data_path / "simple-annotations.zip")
-        ListAnnotationCounterByTask.print_labels_count(
+        ListAnnotationCounterByTask.print_labels_count_csv(
             counter_list,
             output_file=out_path / "list_annotation_count/labels_count_by_task.csv",
             label_columns=["dog", "human"],
         )
 
-    def test_print_attributes_count(self):
+    def test_print_attributes_count_csv(self):
         counter_list = ListAnnotationCounterByTask.get_annotation_counter_list(data_path / "simple-annotations.zip")
-        ListAnnotationCounterByTask.print_attributes_count(
+        ListAnnotationCounterByTask.print_attributes_count_csv(
             counter_list,
             output_file=out_path / "list_annotation_count/attributes_count_by_task.csv",
-            attribute_columns=[("Cat", "occluded", "True"), ("climatic", "temparature", "20")],
+            attribute_columns=[("Cat", "occluded", "true"), ("climatic", "temparature", "20")],
         )
 
 
