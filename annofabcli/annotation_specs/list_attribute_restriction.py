f37d6a7ed989edebcf1e9cde44403a1333365ac8
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-03-09 14:42:29 +0900

    add

--- annofabcli/annotation_specs/list_attribute_restriction.py
+++ annofabcli/annotation_specs/list_attribute_restriction.py
@@ -1,11 +1,10 @@
-"""
-アノテーション仕様を出力する
-"""
-
+from __future__ import annotations
 import argparse
 import logging
 import sys
-from typing import Any, Collection, Dict, List, Optional
+from typing import Any, Collection, Optional
+
+from more_itertools import first_true
 
 import annofabcli
 import annofabcli.common.cli
@@ -17,9 +16,7 @@ from annofabcli.common.cli import (
     build_annofabapi_resource_and_login,
 )
 from annofabcli.common.enums import FormatArgument
-from annofabcli.common.facade import convert_annotation_specs_labels_v2_to_v1
-import annofabapi
-from more_itertools import first_true
+
 logger = logging.getLogger(__name__)
 
 
@@ -32,21 +29,21 @@ class ListAttributeRestrictionMain:
         additionals: アノテーション仕様の属性情報
 
     """
-    def __init__(self, labels:list[dict[str,Any]], additionals:list[dict[str,Any]]):
-        self.attribute_dict = {e["additional_data_definition_id"]:e for e in additionals}
-        self.label_dict = {e["label_id"]:e for e in labels}
 
+    def __init__(self, labels: list[dict[str, Any]], additionals: list[dict[str, Any]]):
+        self.attribute_dict = {e["additional_data_definition_id"]: e for e in additionals}
+        self.label_dict = {e["label_id"]: e for e in labels}
 
-    def get_labels_text(self, label_ids:Collection[str]) -> str:
+    def get_labels_text(self, label_ids: Collection[str]) -> str:
         label_message_list = []
         for label_id in label_ids:
             label = self.label_dict.get(label_id)
             label_name = AnnofabApiFacade.get_label_name_en(label) if label is not None else ""
-            label_message_list.append(f"'{label_name}'(id='{label_id}')")
+            label_message_list.append(f"'{label_name}' (id={label_id})")
 
         return ", ".join(label_message_list)
 
-    def get_object_for_equals_or_notequals(self, value:str, attribute:Optional[dict[str,Any]]) -> str:
+    def get_object_for_equals_or_notequals(self, value: str, attribute: Optional[dict[str, Any]]) -> str:
         """制約条件が `Equals` or `NotEquals`のときの目的語を生成する。
         属性の種類がドロップダウンかセレクトボックスのときは、選択肢の名前を返す。
 
@@ -60,22 +57,22 @@ class ListAttributeRestrictionMain:
         if attribute is not None and attribute["type"] in ["choice", "select"]:
             # ラジオボタンかドロップダウンのとき
             choices = attribute["choices"]
-            choice = first_true(choices, pred=lambda e:e["choice_id"] == value)
+            choice = first_true(choices, pred=lambda e: e["choice_id"] == value)
             if choice is not None:
                 choice_name = AnnofabApiFacade.get_choice_name_en(choice)
-                return f"'{choice_name}'(id='{value}')"
+                return f"'{choice_name}'(id={value})"
             else:
-                return f"''(id='{value}')"
+                return f"''(id={value})"
         else:
             return f"'{value}'"
 
-    def get_restriction_text(self, attribute_id:str, condition:dict[str,Any]) -> str:
+    def get_restriction_text(self, attribute_id: str, condition: dict[str, Any]) -> str:
         attribute = self.attribute_dict.get(attribute_id)
 
         if attribute is not None:
-            subject = f"'{AnnofabApiFacade.get_additional_data_definition_name_en(attribute)}'(id='{attribute_id}',type='{attribute['type']}')"
+            subject = f"'{AnnofabApiFacade.get_additional_data_definition_name_en(attribute)}'(id={attribute_id}, type={attribute['type']})"
         else:
-            subject = f"''(id='{attribute_id}')"
+            subject = f"''(id={attribute_id})"
 
         type = condition["_type"]
 
@@ -97,11 +94,11 @@ class ListAttributeRestrictionMain:
 
         elif type == "Matches":
             verb = "matches"
-            object = condition["value"]
+            object = f"'{condition['value']}'"
 
         elif type == "NotMatches":
             verb = "does not match"
-            object = condition["value"]
+            object = f"'{condition['value']}'"
 
         else:
             pass
@@ -127,84 +124,10 @@ class ListAttributeRestrictionMain:
         return f"{subject} {verb} {object}"
 
 
-
 class ListAttributeRestriction(AbstractCommandLineInterface):
     """
     アノテーション仕様を出力する
     """
-
-    COMMON_MESSAGE = "annofabcli annotation_specs list_label: error:"
-
-    def print_annotation_specs_label(
-        self, project_id: str, arg_format: str, output: Optional[str] = None, history_id: Optional[str] = None
-    ):
-        # [REMOVE_V2_PARAM]
-        annotation_specs, _ = self.service.api.get_annotation_specs(
-            project_id, query_params={"history_id": history_id, "v": "2"}
-        )
-        labels_v1 = convert_annotation_specs_labels_v2_to_v1(
-            labels_v2=annotation_specs["labels"], additionals_v2=annotation_specs["additionals"]
-        )
-        if arg_format == "text":
-            self._print_text_format_labels(labels_v1, output=output)
-
-        elif arg_format in [FormatArgument.JSON.value, FormatArgument.PRETTY_JSON.value]:
-            annofabcli.utils.print_according_to_format(
-                target=labels_v1, arg_format=FormatArgument(arg_format), output=output
-            )
-
-    @staticmethod
-    def _get_name_list(messages: List[Dict[str, Any]]):
-        """
-        日本語名、英語名のリスト（サイズ2）を取得する。日本語名が先頭に格納されている。
-        """
-        ja_name = [e["message"] for e in messages if e["lang"] == "ja-JP"][0]
-        en_name = [e["message"] for e in messages if e["lang"] == "en-US"][0]
-        return [ja_name, en_name]
-
-    @staticmethod
-    def _print_text_format_labels(labels, output: Optional[str] = None):
-        output_lines = []
-        for label in labels:
-            output_lines.append(
-                "\t".join(
-                    [label["label_id"], label["annotation_type"]]
-                    + PrintAnnotationSpecsLabel._get_name_list(label["label_name"]["messages"])
-                )
-            )
-            for additional_data_definition in label["additional_data_definitions"]:
-                output_lines.append(
-                    "\t".join(
-                        [
-                            "",
-                            additional_data_definition["additional_data_definition_id"],
-                            additional_data_definition["type"],
-                        ]
-                        + PrintAnnotationSpecsLabel._get_name_list(additional_data_definition["name"]["messages"])
-                    )
-                )
-                if additional_data_definition["type"] in ["choice", "select"]:
-                    for choice in additional_data_definition["choices"]:
-                        output_lines.append(
-                            "\t".join(
-                                ["", "", choice["choice_id"], ""]
-                                + PrintAnnotationSpecsLabel._get_name_list(choice["name"]["messages"])
-                            )
-                        )
-
-        annofabcli.utils.output_string("\n".join(output_lines), output)
-
-    def get_history_id_from_before_index(self, project_id: str, before: int) -> Optional[str]:
-        histories, _ = self.service.api.get_annotation_specs_histories(project_id)
-        if before + 1 > len(histories):
-            logger.warning(f"アノテーション仕様の履歴は{len(histories)}個のため、最新より{before}個前のアノテーション仕様は見つかりませんでした。")
-            return None
-        history = histories[-(before + 1)]
-        logger.info(
-            f"{history['updated_datetime']}のアノテーション仕様を出力します。"
-            f"history_id={history['history_id']}, comment={history['comment']}"
-        )
-        return history["history_id"]
 
     def main(self):
 
@@ -274,7 +197,7 @@ def parse_args(parser: argparse.ArgumentParser):
 def main(args):
     service = build_annofabapi_resource_and_login(args)
     facade = AnnofabApiFacade(service)
-    PrintAnnotationSpecsLabel(service, facade, args).main()
+    ListAttributeRestriction(service, facade, args).main()
 
 
 def add_parser(subparsers: Optional[argparse._SubParsersAction] = None):
---
+++ tests/test_local_annotation_specs.py
@@ -0,0 +1,33 @@
+import json
+import os
+from pathlib import Path
+
+from annofabcli.__main__ import main
+from annofabcli.annotation_specs.list_attribute_restriction import ListAttributeRestrictionMain
+
+# プロジェクトトップに移動する
+os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/../")
+
+data_dir = Path("./tests/data/annotation_specs")
+out_dir = Path("./tests/out/annotation_specs")
+out_dir.mkdir(exist_ok=True, parents=True)
+
+# プロジェクトトップに移動する
+os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/../")
+
+
+class TestListAttributeRestrictionMain:
+    @classmethod
+    def setup_class(cls):
+        with (data_dir / "annotation_specs.json").open() as f:
+            annotation_specs = json.load(f)
+
+        cls.obj = ListAttributeRestrictionMain(
+            labels=annotation_specs["labels"], additionals=annotation_specs["additionals"]
+        )
+        cls.annotation_specs = annotation_specs
+
+    def test_get_restriction_text(self):
+        for restriction in self.annotation_specs["restrictions"]:
+            actual = self.obj.get_restriction_text(restriction["additional_data_definition_id"], restriction["condition"])
+            print(actual)
