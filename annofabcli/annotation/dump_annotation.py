2b16ea7533c7ff19327122ded07465e2a95f5497
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-05-12 22:33:39 +0900

    format

--- annofabcli/annotation/dump_annotation.py
+++ annofabcli/annotation/dump_annotation.py
@@ -10,12 +10,14 @@ from annofabapi.models import AnnotationDataHoldingType
 import annofabcli
 from annofabcli import AnnofabApiFacade
 from annofabcli.common.cli import AbstractCommandLineInterface, ArgumentParser, build_annofabapi_resource_and_login
+import annofabapi
 
 logger = logging.getLogger(__name__)
 
 
 class DumpAnnotationMain:
-    def __init__(self, project_id: str):
+    def __init__(self, service: annofabapi.Resource, project_id: str):
+        self.service = service
         self.project_id = project_id
 
     def dump_annotation_for_input_data(self, task_id: str, input_data_id: str, task_dir: Path) -> None:
@@ -76,8 +78,7 @@ class DumpAnnotationMain:
     def dump_annotation(self, task_id_list: List[str], output_dir: Path):
         super().validate_project(self.project_id, project_member_roles=None)
 
-        project_title = self.facade.get_project_title(self.project_id)
-        logger.info(f"プロジェクト'{project_title}'に対して、タスク{len(task_id_list)} 件のアノテーションをファイルに保存します。")
+        logger.info(f"project_id='{self.project_id=}'のプロジェクトのタスク{len(task_id_list)} 件のアノテーションをファイルに保存します。")
 
         output_dir.mkdir(exist_ok=True, parents=True)
 
