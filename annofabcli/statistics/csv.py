5c0e1b980984918ba040d3612d3b5c870cb1baca
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-06-15 23:06:21 +0900

    全体の生産性のファイル名を移動

--- annofabcli/statistics/csv.py
+++ annofabcli/statistics/csv.py
@@ -2,7 +2,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-FILENAME_WHOLE_PERFORMANCE = "全体の生産性と品質.csv"
 FILENAME_PERFORMANCE_PER_USER = "メンバごとの生産性と品質.csv"
 
 FILENAME_PERFORMANCE_PER_DATE = "日毎の生産量と生産性.csv"
