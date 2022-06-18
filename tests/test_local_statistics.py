c24aab63e22eff00d773ea380d4e91151e4ded70
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-06-18 10:59:41 +0900

    update

--- tests/test_local_statistics.py
+++ tests/test_local_statistics.py
@@ -461,7 +461,6 @@ class TestProjectPerformance:
         row = df.iloc[0]
         # メインの項目をアサートする
         assert row[("dirname", "")] == "empty"
-        print(f"{row[('project_title', '')]=}")
         assert numpy.isnan(row[("project_title", "")])
         assert numpy.isnan(row[("start_date", "")])
         assert numpy.isnan(row[("actual_worktime_hour", "sum")])
