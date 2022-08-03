6685e08322e713e3d0f2dd9c5c811fb6f8863061
PFE <67464161+pfe171@users.noreply.github.com>
2022-08-03 10:47:51 +0900

    タスクのステータスを作業中から休憩中にするコマンドの作成 (#835)
    * change_statusコマンド作成
    
    * ChangeStatusMainクラスを作成
    
    * 変更後のタスクの状態をstr型として保持させる
    
    * 状態 -> ステータス　で統一
    
    * change_status関数の中身を実装（ログ、並列処理）
    
    * ラッパー実装
    
    * ログ
    
    * タスクが作業中かどうか判定
    
    * 確認ログ
    
    * タスクがクエリにマッチするか判定
    
    * apiを呼び出しステータスを変更
    
    * 権限を確認
    
    * ログを微調整
    
    * 引数の順序を修正
    
    * 操作を作業中→休憩中に限定
    
    * change_status.py削除
    
    * クラス名: changestatus -> changestatustobreak
    
    * linter
    
    * ドキュメント追加
    
    * ドキュメント修正
    
    * `AbstractCommandLineWithConfirmInterface`クラスを継承する
    
    * 保留中 -> 休憩中の操作に対応
    
    * ドキュメント修正
    
    * Update annofabcli/task/change_status_to_break.py
    
    Co-authored-by: yuji38kwmt <yuji38kwmt@yahoo.co.jp>
    
    * Update annofabcli/task/change_status_to_break.py
    
    Co-authored-by: yuji38kwmt <yuji38kwmt@yahoo.co.jp>
    
    * コード整形
    
    Co-authored-by: yuji38kwmt <yuji38kwmt@yahoo.co.jp>

--- annofabcli/task/subcommand_task.py
+++ annofabcli/task/subcommand_task.py
@@ -5,6 +5,7 @@ import annofabcli
 import annofabcli.common.cli
 import annofabcli.task.cancel_acceptance
 import annofabcli.task.change_operator
+import annofabcli.task.change_status_to_break
 import annofabcli.task.complete_tasks
 import annofabcli.task.copy_tasks
 import annofabcli.task.delete_tasks
@@ -25,6 +26,7 @@ def parse_args(parser: argparse.ArgumentParser):
     # サブコマンドの定義
     annofabcli.task.cancel_acceptance.add_parser(subparsers)
     annofabcli.task.change_operator.add_parser(subparsers)
+    annofabcli.task.change_status_to_break.add_parser(subparsers)
     annofabcli.task.complete_tasks.add_parser(subparsers)
     annofabcli.task.copy_tasks.add_parser(subparsers)
     annofabcli.task.delete_tasks.add_parser(subparsers)
