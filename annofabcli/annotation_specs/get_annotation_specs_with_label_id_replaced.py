cf597b45f03f8bb4eba5a62724796d3a8189b68e
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-08-28 23:48:03 +0900

    コマンド作成

--- annofabcli/annotation_specs/get_annotation_specs_with_label_id_replaced.py
+++ annofabcli/annotation_specs/get_annotation_specs_with_label_id_replaced.py
@@ -1,52 +1,43 @@
 from __future__ import annotations
+
 import argparse
 import logging
-from multiprocessing.sharedctypes import Value
-import sys
-from typing import Any, Dict, List, Optional
+import re
+from typing import Any, Collection, Optional
+
+import more_itertools
 
 import annofabcli
 import annofabcli.common.cli
 from annofabcli.common.cli import (
-    COMMAND_LINE_ERROR_STATUS_CODE,
-    AbstractCommandLineInterface,
-    ArgumentParser,
-    build_annofabapi_resource_and_login,
-)
-from annofabcli.common.enums import FormatArgument
-from annofabcli.common.facade import AnnofabApiFacade, convert_annotation_specs_labels_v2_to_v1
-
-from annofabcli.common.cli import (
-    COMMAND_LINE_ERROR_STATUS_CODE,
     AbstractCommandLineInterface,
     AbstractCommandLineWithConfirmInterface,
     ArgumentParser,
     build_annofabapi_resource_and_login,
-    get_json_from_args,
+    get_list_from_args,
 )
-from typing import Collection
+from annofabcli.common.enums import FormatArgument
+from annofabcli.common.facade import AnnofabApiFacade
 
 logger = logging.getLogger(__name__)
 
 
 class ReplacingLabelId(AbstractCommandLineWithConfirmInterface):
-
-
-
-    def replace_label_id_of_restrictions(self, old_label_id, new_label_id, restrictions:list[dict[str,Any]]) -> None:
+    @staticmethod
+    def replace_label_id_of_restrictions(old_label_id, new_label_id, restriction_list: list[dict[str, Any]]) -> None:
         """
-        制約情報の中で使用されているlabel_idを置換する。
+        制約情報の中で使用されているlabel_idを新しいlabel_idに変更する
+
+        Args:
+            old_label_id: 変更前のlabel_id
+            new_label_id: 変更後のlabel_id
+            restriction_list: (IN/OUT) 制約情報。制約情報のlabel_idが変更される。
         """
-        def _replace_label_id_in_condition(condition:dict[str,Any]) -> None:
-            """
-            制約情報の中のlabel_idを置換する
 
-            Args:
-
-            """
+        def _replace_label_id_in_condition(condition: dict[str, Any]) -> None:
             if condition["_type"] == "Imply":
-                self._replace_label_id_of_restriction(old_label_id, new_label_id, condition["premise"]["condition"])
-                self._replace_label_id_of_restriction(old_label_id, new_label_id, condition["condition"])
+                _replace_label_id_in_condition(condition["premise"]["condition"])
+                _replace_label_id_in_condition(condition["condition"])
             elif condition["_type"] == "HasLabel":
                 # アノテーションリンク先であるラベルIDを置換する
                 labels = condition["labels"]
@@ -57,58 +48,78 @@ class ReplacingLabelId(AbstractCommandLineWithConfirmInterface):
                     # old_label_idがlabelsになければ、何もしない
                     pass
 
-        for restriction in restrictions:
+        for restriction in restriction_list:
             _replace_label_id_in_condition(restriction["condition"])
 
+    @staticmethod
+    def exists_label_with_label_id(label_list: list[dict[str, Any]], label_id: str) -> bool:
+        """指定したlabel_idを持つラベル情報が存在するか否か"""
+        return more_itertools.first_true(label_list, pred=lambda e: e["label_id"] == label_id) is not None
 
-    def replace_label_id(self, label:dict[str,Any]) -> None:
-        label_id = label["label_id"]
-        label_name_en = AnnofabApiFacade.get_label_name_en(label)
+    @staticmethod
+    def validate_label_id(str_id: str) -> bool:
+        """
+        指定した文字列が、label_idに利用できる文字列か否か
+        """
+        return re.fullmatch("[A-Za-z0-9_.\-]+", str_id) is not None
 
-        # TODO confirm
-        # TODO validate
+    def main(self, annotation_specs: dict[str, Any], *, target_label_names: Optional[Collection[str]] = None) -> None:
+        """
+        アノテーション仕様内のlabel_idをラベル英語名に変更します。
 
-        label["label_id"] = label_name
+        Args:
+            annotation_specs: (IN/OUT) アノテーション仕様情報。中身が変更されます。
+            target_label_names: 変更対象のラベル英語名。Noneならすべてのラベルのlabel_idを変更します。
 
-        # TODO
-        # アノテーションリンクと属性
-        # HasLabel制約
-
-    def main(self, annotation_specs:dict[str,Any], target_label_names:Optional[Collection[str]]=None) -> dict[str, Any]:
+        """
         label_list = annotation_specs["labels"]
+        restriction_list = annotation_specs["restrictions"]
 
+        for label in label_list:
+            label_id = label["label_id"]
+            label_name_en = AnnofabApiFacade.get_label_name_en(label)
+
+            if target_label_names is not None:
+                if label_name_en not in target_label_names:
+                    continue
+
+            if not self.validate_label_id(label_name_en):
+                logger.warning(
+                    f"label_name_en='{label_name_en}'はlabel_idにできない文字列であるため、label_id='{label_id}'を'{label_name_en}'に変更しません。"
+                )
+                continue
+
+            if self.exists_label_with_label_id(label_list, label_id=label_name_en):
+                logger.warning(
+                    f"label_id='{label_name_en}'であるラベルが既に存在するため、label_id='{label_id}'を'{label_name_en}'に変更しません。"
+                )
+                continue
+
+            if not self.confirm_processing(f"label_id='{label_id}'を'{label_name_en}'に変更しますか？"):
+                continue
+
+            # ラベル内のlabel_idを変更する
+            label["label_id"] = label_name_en
+            self.replace_label_id_of_restrictions(
+                old_label_id=label_id, new_label_id=label_name_en, restriction_list=restriction_list
+            )
 
 
 class GetAnnotationSpecsWithLabelIdReplaced(AbstractCommandLineInterface):
-
     def main(self):
 
         args = self.args
         project_id: str = args.project_id
         super().validate_project(project_id)
 
-        annotation_specs = self.service.api.get_annotation_specs(project_id, query_params={"v":3})
+        annotation_specs, _ = self.service.api.get_annotation_specs(project_id, query_params={"v": 3})
 
-        target_task_ids = (
-            set(annofabcli.common.cli.get_list_from_args(args.task_id)) if args.task_id is not None else None
-        )
+        target_label_names = set(get_list_from_args(args.label_name)) if args.label_name is not None else None
 
+        # アノテーション仕様のlabel_idを変更する
+        ReplacingLabelId(all_yes=args.yes).main(annotation_specs, target_label_names=target_label_names)
 
-        if args.before is not None:
-            history_id = self.get_history_id_from_before_index(args.project_id, args.before)
-            if history_id is None:
-                print(
-                    f"{self.COMMON_MESSAGE} argument --before: 最新より{args.before}個前のアノテーション仕様は見つかりませんでした。",
-                    file=sys.stderr,
-                )
-                sys.exit(COMMAND_LINE_ERROR_STATUS_CODE)
-        else:
-            # args.beforeがNoneならば、必ずargs.history_idはNoneでない
-            history_id = args.history_id
-
-        self.print_annotation_specs_label(
-            args.project_id, arg_format=args.format, output=args.output, history_id=history_id
-        )
+        self.print_according_to_format(annotation_specs)
 
 
 def parse_args(parser: argparse.ArgumentParser):
@@ -125,13 +136,21 @@ def parse_args(parser: argparse.ArgumentParser):
 
     argument_parser.add_output()
 
+    argument_parser.add_format(
+        choices=[
+            FormatArgument.JSON,
+            FormatArgument.PRETTY_JSON,
+        ],
+        default=FormatArgument.PRETTY_JSON,
+    )
+
     parser.set_defaults(subcommand_func=main)
 
 
 def main(args):
     service = build_annofabapi_resource_and_login(args)
     facade = AnnofabApiFacade(service)
-    PrintAnnotationSpecsLabel(service, facade, args).main()
+    GetAnnotationSpecsWithLabelIdReplaced(service, facade, args).main()
 
 
 def add_parser(subparsers: Optional[argparse._SubParsersAction] = None):
@@ -139,10 +158,11 @@ def add_parser(subparsers: Optional[argparse._SubParsersAction] = None):
 
     subcommand_help = "label_idをUUIDから英語名に置換したアノテーション仕様のJSONを取得します。"
 
-    description =( "label_idをUUIDから英語名に置換したアノテーション仕様のJSONを取得します。\n"
-            "アノテーション仕様は変更しません。画面のインポート機能を使って、アノテーション仕様を変更することを想定しています。\n"
-            "【注意】既にアノテーションが存在する状態でlabel_idを変更すると、既存のアノテーション情報が消える恐れがあります。十分注意して、label_idを変更してください。")
-
+    description = (
+        "label_idをUUIDから英語名に置換したアノテーション仕様のJSONを取得します。\n"
+        "アノテーション仕様は変更しません。画面のインポート機能を使って、アノテーション仕様を変更することを想定しています。\n"
+        "【注意】既にアノテーションが存在する状態でlabel_idを変更すると、既存のアノテーション情報が消える恐れがあります。十分注意して、label_idを変更してください。"
+    )
 
     parser = annofabcli.common.cli.add_parser(subparsers, subcommand_name, subcommand_help, description=description)
     parse_args(parser)
--- annofabcli/annotation_specs/subcommand_annotation_specs.py
+++ annofabcli/annotation_specs/subcommand_annotation_specs.py
@@ -2,6 +2,7 @@ import argparse
 from typing import Optional
 
 import annofabcli
+import annofabcli.annotation_specs.get_annotation_specs_with_label_id_replaced
 import annofabcli.annotation_specs.list_annotation_specs_history
 import annofabcli.annotation_specs.list_annotation_specs_label
 import annofabcli.annotation_specs.list_attribute_restriction
@@ -14,6 +15,7 @@ def parse_args(parser: argparse.ArgumentParser):
     subparsers = parser.add_subparsers(dest="subcommand_name")
 
     # サブコマンドの定義
+    annofabcli.annotation_specs.get_annotation_specs_with_label_id_replaced.add_parser(subparsers)
     annofabcli.annotation_specs.list_attribute_restriction.add_parser(subparsers)
     annofabcli.annotation_specs.list_annotation_specs_history.add_parser(subparsers)
     annofabcli.annotation_specs.list_annotation_specs_label.add_parser(subparsers)
