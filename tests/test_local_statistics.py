ffd673c85766bcb35a04f273e20d5f01b8b325b3
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2022-10-17 14:21:17 +0900

    test_list_worktimeの追加

--- tests/test_local_statistics.py
+++ tests/test_local_statistics.py
@@ -3,10 +3,8 @@ from pathlib import Path
 
 import pandas
 
-from annofabcli.statistics.list_worktime import WorktimeFromTaskHistoryEvent, get_df_worktime
 from annofabcli.statistics.summarize_task_count_by_task_id_group import create_task_count_summary_df, get_task_id_prefix
 from annofabcli.statistics.table import Table
-from annofabcli.task_history_event.list_worktime import SimpleTaskHistoryEvent
 
 out_path = Path("./tests/out/statistics")
 data_path = Path("./tests/data/statistics")
@@ -56,47 +54,3 @@ class TestSummarizeTaskCountByTaskId:
         df = create_task_count_summary_df(task_list, task_id_delimiter="_", task_id_groups=None)
         assert len(df) == 1
         df.iloc[0]["task_id_group"] == "sample"
-
-
-class TestListWorktime:
-    def test_get_df_worktime(self):
-        event_list = [
-            WorktimeFromTaskHistoryEvent(
-                project_id="prj1",
-                task_id="task1",
-                phase="annotation",
-                phase_stage=1,
-                account_id="alice",
-                user_id="alice",
-                username="Alice",
-                worktime_hour=3.0,
-                start_event=SimpleTaskHistoryEvent(
-                    task_history_id="unknown", created_datetime="2019-01-01T23:00:00.000+09:00", status="working"
-                ),
-                end_event=SimpleTaskHistoryEvent(
-                    task_history_id="unknown", created_datetime="2019-01-02T02:00:00.000+09:00", status="on_holding"
-                ),
-            ),
-            WorktimeFromTaskHistoryEvent(
-                project_id="prj1",
-                task_id="task2",
-                phase="acceptance",
-                phase_stage=1,
-                account_id="bob",
-                user_id="bob",
-                username="Bob",
-                worktime_hour=1.0,
-                start_event=SimpleTaskHistoryEvent(
-                    task_history_id="unknown", created_datetime="2019-01-03T22:00:00.000+09:00", status="working"
-                ),
-                end_event=SimpleTaskHistoryEvent(
-                    task_history_id="unknown", created_datetime="2019-01-03T23:00:00.000+09:00", status="on_holding"
-                ),
-            ),
-        ]
-
-        member_list = [
-            {"account_id": "alice", "user_id": "alice", "username": "Alice", "biography": "U.S."},
-            {"account_id": "bob", "user_id": "bob", "username": "Bob", "biography": "Japan"},
-        ]
-        df = get_df_worktime(event_list, member_list)
