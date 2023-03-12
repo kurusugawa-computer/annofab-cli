652ac04b0db36edafa4bfdadf61d3d3ee9d5bcc3
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2023-03-13 07:37:11 +0900

    PIE802 適用

--- tests/common/test_cli.py
+++ tests/common/test_cli.py
@@ -12,10 +12,10 @@ def test_get_list_from_args():
     expected = ["id1", "id2", "id3"]
 
     actual = get_list_from_args(["id1", "id2", "id3"])
-    assert all([a == b for a, b in zip(actual, expected)])
+    assert all(a == b for a, b in zip(actual, expected))
 
     actual = get_list_from_args(["file://tests/data/arg_id_list.txt"])
-    assert all([a == b for a, b in zip(actual, expected)])
+    assert all(a == b for a, b in zip(actual, expected))
 
     actual = get_list_from_args(None)
     assert len(actual) == 0
