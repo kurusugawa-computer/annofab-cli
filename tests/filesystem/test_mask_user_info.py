6db0cac8df3afc68936a437177262e76357dd0af
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2023-03-13 09:46:12 +0900

    mask_user_infoのテストコードを修正

--- tests/filesystem/test_mask_user_info.py
+++ tests/filesystem/test_mask_user_info.py
@@ -16,36 +16,93 @@ class TestMaskUserInfo:
     def test_create_masked_name(self):
         assert create_masked_name("abc") == "P"
 
-    def test_create_masked_user_info_df(self):
+    def test_create_masked_user_info_df__オプショナル引数を指定しない(self):
         # ヘッダ２行のCSVを読み込む
-        df = create_masked_user_info_df(read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2))
+        df_original = read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2)
+        df_masked = create_masked_user_info_df(df_original)
+        expected_masked_user_info = ["FF", "AA", "IK"]
+        assert list(df_masked["user_id"]) == expected_masked_user_info
+        assert list(df_masked["username"]) == expected_masked_user_info
+        assert list(df_masked["account_id"]) == expected_masked_user_info
+        assert list(df_masked["biography"]) == ["category-TD", "category-TD", "category-TU"]
 
-        df = create_masked_user_info_df(
-            read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2),
+        # ユーザー情報以外は変わっていないことの確認
+        assert df_masked["task_count"].equals(df_original["task_count"])
+
+    def test_create_masked_user_info_df__not_masked_biography_set引数を指定する(self):
+        df_original = read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2)
+        df_masked = create_masked_user_info_df(
+            df_original,
             not_masked_biography_set={"China"},
-            not_masked_user_id_set=None,
         )
 
-        df = create_masked_user_info_df(
-            read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2),
-            not_masked_biography_set=None,
-            not_masked_user_id_set={"alice"},
+        # biographyが"China"の行はマスクされていないこと、それ以外はマスクされていることの確認
+        not_masked_row_index = 2
+        assert list(df_masked["user_id"]) == ["FF", "AA", df_original.iloc[not_masked_row_index][("user_id", "")]]
+        assert list(df_masked["username"]) == ["FF", "AA", df_original.iloc[not_masked_row_index][("username", "")]]
+        assert list(df_masked["account_id"]) == ["FF", "AA", df_original.iloc[not_masked_row_index][("account_id", "")]]
+        assert list(df_masked["biography"]) == [
+            "category-TD",
+            "category-TD",
+            df_original.iloc[not_masked_row_index][("biography", "")],
+        ]
+
+    def test_create_masked_user_info_df__not_masked_user_id_set引数を指定する(self):
+        df_original = read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2)
+        df_masked = create_masked_user_info_df(df_original, not_masked_user_id_set={"alice"})
+
+        # user_idが"alice"の行はマスクされていないこと、それ以外はマスクされていることの確認
+        not_masked_row_index = 0
+        assert list(df_masked["user_id"]) == [df_original.iloc[not_masked_row_index][("user_id", "")], "AA", "IK"]
+        assert list(df_masked["username"]) == [df_original.iloc[not_masked_row_index][("username", "")], "AA", "IK"]
+        assert list(df_masked["account_id"]) == [df_original.iloc[not_masked_row_index][("account_id", "")], "AA", "IK"]
+        assert list(df_masked["biography"]) == [
+            df_original.iloc[not_masked_row_index][("biography", "")],
+            "category-TD",
+            "category-TU",
+        ]
+
+    def test_create_masked_user_info_df__not_masked_user_id_setとnot_masked_biography_set引数を指定する(self):
+        df_original = read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2)
+        df_masked = create_masked_user_info_df(
+            df_original, not_masked_biography_set={"China"}, not_masked_user_id_set={"bob"}
         )
 
-        df = create_masked_user_info_df(
-            read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2),
-            not_masked_biography_set={"China"},
-            not_masked_user_id_set={"chris"},
-        )
+        # user_idが"bob", biographyが"China"の行はマスクされていないこと、それ以外はマスクされていることの確認
+        not_masked_row_indexes = [1, 2]
+        assert list(df_masked["user_id"]) == ["FF", *list(df_original.iloc[not_masked_row_indexes][("user_id", "")])]
+        assert list(df_masked["username"]) == ["FF", *list(df_original.iloc[not_masked_row_indexes][("username", "")])]
+        assert list(df_masked["account_id"]) == [
+            "FF",
+            *list(df_original.iloc[not_masked_row_indexes][("account_id", "")]),
+        ]
+        assert list(df_masked["biography"]) == [
+            "category-TD",
+            *list(df_original.iloc[not_masked_row_indexes][("biography", "")]),
+        ]
 
-    def test_create_masked_user_info_df2(self):
+    def test_create_masked_user_info_df__user_idしかないCSVをマスクする(self):
         # ヘッダ２行のCSVを読み込む。ただし username, biography, account_id がないCSV
-        df = create_masked_user_info_df(
-            read_multiheader_csv(str(data_dir / "user2_1.csv"), header_row_count=2),
+        df_original = read_multiheader_csv(str(data_dir / "user2.csv"), header_row_count=2)
+        df_masked = create_masked_user_info_df(
+            df_original,
             not_masked_biography_set={"China"},
-            not_masked_user_id_set={"chris"},
+            not_masked_user_id_set={"bob"},
         )
+        # user_idが"bob", biographyが"China"の行はマスクされていないこと、それ以外はマスクされていることの確認
+        not_masked_row_indexes = [1, 2]
+        assert list(df_masked["user_id"]) == ["FF", *list(df_original.iloc[not_masked_row_indexes][("user_id", "")])]
 
-    def test_create_masked_user_info_df3(self):
+    def test_create_masked_user_info_df__ヘッダが1行のCSVをマスクする(self):
         # ヘッダ1行のCSVを読み込む
-        df = create_masked_user_info_df(pandas.read_csv(str(data_dir / "user1.csv")))
+        df_original = pandas.read_csv(str(data_dir / "user1.csv"))
+        df_masked = create_masked_user_info_df(df_original)
+
+        expected_masked_user_info = ["FF", "AA", "IK"]
+        assert list(df_masked["user_id"]) == expected_masked_user_info
+        assert list(df_masked["username"]) == expected_masked_user_info
+        assert list(df_masked["account_id"]) == expected_masked_user_info
+        assert list(df_masked["biography"]) == ["category-TD", "category-TD", "category-TU"]
+
+        # ユーザー情報以外は変わっていないことの確認
+        assert df_masked["task_count"].equals(df_original["task_count"])
