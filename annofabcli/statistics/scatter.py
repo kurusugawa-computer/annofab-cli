77d8d9cacbba5bf475386caa9317e0c434497875
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-03-20 17:11:35 +0900

    plot_quality_and_productivityのリファクタリング

--- annofabcli/statistics/scatter.py
+++ annofabcli/statistics/scatter.py
@@ -31,110 +31,6 @@ def get_color_from_palette(index: int) -> str:
     return my_palette[index % len(my_palette)]
 
 
-def plot_bubble(
-    fig: figure,
-    source: ColumnDataSource,
-    x_column_name: str,
-    y_column_name: str,
-    text_column_name: str,
-    size_column_name: str,
-    legend_label: str,
-    color: str,
-    func_get_bubble_size: Optional[Callable[[Any], int]] = None,
-) -> None:
-    """
-    バブルチャート用にプロットする。
-
-    Args:
-        fig:
-        source:
-        x_column_name: sourceに対応するX軸の列名
-        y_column_name: sourceに対応するY軸の列名
-        legend_label: 凡例に表示する名前
-        color: 線と点の色
-
-    """
-
-    def _worktime_hour_to_scatter_size(worktime_hour: float) -> int:
-        """
-        作業時間からバブルの大きさに変更する。
-        """
-        MIN_SCATTER_SIZE = 4
-        if worktime_hour <= 0:
-            return MIN_SCATTER_SIZE
-        tmp = int(math.log2(worktime_hour) * 15 - 50)
-        if tmp < MIN_SCATTER_SIZE:
-            return MIN_SCATTER_SIZE
-        else:
-            return tmp
-
-    if func_get_bubble_size is None:
-        func_get_bubble_size = _worktime_hour_to_scatter_size
-
-    if legend_label == "":
-        legend_label = "none"
-
-    tmp_size_field = f"__size__{size_column_name}"
-    source.data[tmp_size_field] = numpy.array(list(map(func_get_bubble_size, source.data[size_column_name])))
-    fig.scatter(
-        x=x_column_name,
-        y=y_column_name,
-        source=source,
-        legend_label=legend_label,
-        color=color,
-        fill_alpha=0.5,
-        muted_alpha=0.2,
-        size=tmp_size_field,
-    )
-    fig.text(
-        x=x_column_name,
-        y=y_column_name,
-        source=source,
-        text=text_column_name,
-        text_align="center",
-        text_baseline="middle",
-        text_font_size="7pt",
-        legend_label=legend_label,
-        muted_alpha=0.2,
-    )
-
-
-def plot_scatter(
-    fig: figure,
-    source: ColumnDataSource,
-    x_column_name: str,
-    y_column_name: str,
-    text_column_name: str,
-    legend_label: str,
-    color: str,
-) -> None:
-    """
-    丸でプロットして、ユーザ名を表示する。
-
-    Args:
-        fig:
-        source:
-        x_column_name: sourceに対応するX軸の列名
-        y_column_name: sourceに対応するY軸の列名
-        legend_label: 凡例に表示する名前
-        color: 線と点の色
-
-    """
-    if legend_label == "":
-        legend_label = "none"
-
-    fig.circle(x=x_column_name, y=y_column_name, source=source, legend_label=legend_label, color=color, muted_alpha=0.2)
-    fig.text(
-        x=x_column_name,
-        y=y_column_name,
-        source=source,
-        text=text_column_name,
-        text_font_size="7pt",
-        legend_label=legend_label,
-        muted_alpha=0.2,
-    )
-
-
 def create_hover_tool(tool_tip_items: list[str]) -> HoverTool:
     """
     HoverTool用のオブジェクトを生成する。
@@ -254,6 +150,80 @@ class ScatterGraph:
                 legend_label=legend_label,
                 text_font_style="normal",
                 text_font_size="7pt",
+            )
+
+    def plot_bubble(
+        self,
+        source: ColumnDataSource,
+        x_column_name: str,
+        y_column_name: str,
+        username_column_name: str,
+        user_id_column_name: str,
+        size_column_name: str,
+        legend_label: str,
+        color: str,
+        *,
+        func_get_bubble_size: Optional[Callable[[Any], int]] = None,
+    ) -> None:
+        """
+        バブルチャート用にプロットする。
+
+        Args:
+            source:
+            x_column_name: sourceに対応するX軸の列名
+            y_column_name: sourceに対応するY軸の列名
+            legend_label: 凡例に表示する名前
+            color: 線と点の色
+
+        """
+
+        def _worktime_hour_to_scatter_size(worktime_hour: float) -> int:
+            """
+            作業時間からバブルの大きさに変更する。
+            """
+            MIN_SCATTER_SIZE = 4
+            if worktime_hour <= 0:
+                return MIN_SCATTER_SIZE
+            tmp = int(math.log2(worktime_hour) * 15 - 50)
+            if tmp < MIN_SCATTER_SIZE:
+                return MIN_SCATTER_SIZE
+            else:
+                return tmp
+
+        if func_get_bubble_size is None:
+            func_get_bubble_size = _worktime_hour_to_scatter_size
+
+        if legend_label == "":
+            legend_label = "none"
+
+        tmp_size_field = f"__size__{size_column_name}"
+        source.data[tmp_size_field] = numpy.array(list(map(func_get_bubble_size, source.data[size_column_name])))
+
+        self._scatter_glyphs[legend_label] = self.figure.scatter(
+            x=x_column_name,
+            y=y_column_name,
+            source=source,
+            legend_label=legend_label,
+            color=color,
+            fill_alpha=0.5,
+            muted_alpha=0.2,
+            size=tmp_size_field,
+        )
+
+        for x, y, username, user_id in zip(
+            source.data[x_column_name], source.data[y_column_name], source.data[username_column_name], source.data[user_id_column_name]
+        ):
+            self.text_glyphs[user_id] = self.figure.text(
+                x=x,
+                y=y,
+                # listで渡している理由: https://qiita.com/yuji38kwmt/items/5fad41f6db0090a80af4
+                text=[username],
+                legend_label=legend_label,
+                text_font_style="normal",
+                text_font_size="7pt",
+                text_align="center",
+                text_baseline="middle",
+                muted_alpha=0.2,
             )
 
     def process_after_adding_glyphs(self) -> None:
