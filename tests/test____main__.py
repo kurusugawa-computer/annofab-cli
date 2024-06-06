2c24a1085500107f1c069f58151a5a5d47eca81b
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2024-06-06 16:39:19 +0900

    sensitiveな情報をログに出力しないようにする

---
+++ tests/test____main__.py
@@ -0,0 +1,13 @@
+from annofabcli.__main__ import mask_sensitive_value_in_argv
+
+
+def test__mask_sensitive_value_in_argv():
+    actual = mask_sensitive_value_in_argv(["--annofab_user_id", "alice", "--annofab_password", "pw"])
+    assert actual == ["--annofab_user_id", "***", "--annofab_password", "***"]
+
+
+def test__mask_sensitive_value_in_argv__同じ引数を指定する():
+    actual = mask_sensitive_value_in_argv(
+        ["--annofab_user_id", "alice", "--annofab_password", "pw_alice", "--annofab_user_id", "bob", "--annofab_password", "pw_bob"]
+    )
+    assert actual == ["--annofab_user_id", "***", "--annofab_password", "***", "--annofab_user_id", "***", "--annofab_password", "***"]
