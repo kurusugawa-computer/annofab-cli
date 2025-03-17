d2958a49841240abe23adcb6843ff7b6ffd68fe6
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2025-03-17 18:02:29 +0900

    format

--- annofabcli/supplementary/delete_supplementary_data.py
+++ annofabcli/supplementary/delete_supplementary_data.py
@@ -30,19 +30,13 @@ key: input_data_id, value: supplementary_data_idのList
 """
 
 
+
 def get_input_data_supplementary_data_dict_from_csv(csv_path: Path) -> InputDataSupplementaryDataDict:
-    df = pandas.read_csv(
-        str(csv_path),
-        sep=",",
-        header=None,
-        names=[
-            "input_data_id",
-            "supplementary_data_id",
-        ],
-        # IDは必ず文字列として読み込むようにする
-        dtype={"input_data_id": str, "supplementary_data_id": str},
+    df: pandas.DataFrame = pandas.read_csv(
+        csv_path,
+        dtype={"input_data_id": "string", "supplementary_data_id": "string"},
     )
-    input_data_dict = defaultdict(list)
+    input_data_dict: InputDataSupplementaryDataDict = defaultdict(list)
     for input_data_id, supplementary_data_id in zip(df["input_data_id"], df["supplementary_data_id"]):
         input_data_dict[input_data_id].append(supplementary_data_id)
     return input_data_dict
@@ -58,7 +52,7 @@ def get_input_data_supplementary_data_dict_from_list(supplementary_data_list: li
 
 
 class DeleteSupplementaryDataMain(CommandLineWithConfirm):
-    def __init__(self, service: annofabapi.Resource, all_yes: bool = False) -> None:  # noqa: FBT001, FBT002
+    def __init__(self, service: annofabapi.Resource, *, all_yes: bool = False) -> None:
         self.service = service
         self.facade = AnnofabApiFacade(service)
         CommandLineWithConfirm.__init__(self, all_yes)
@@ -122,7 +116,7 @@ class DeleteSupplementaryDataMain(CommandLineWithConfirm):
                 continue
         return deleted_count
 
-    def delete_supplementary_data_list(self, project_id: str, input_data_dict: InputDataSupplementaryDataDict):  # noqa: ANN201
+    def delete_supplementary_data_list(self, project_id: str, input_data_dict: InputDataSupplementaryDataDict) -> None:
         deleted_count = 0
         total_count = sum(len(e) for e in input_data_dict.values())
         for input_data_id, supplementary_data_id_list in input_data_dict.items():
@@ -168,7 +162,7 @@ class DeleteSupplementaryDataMain(CommandLineWithConfirm):
 
         return deleted_count
 
-    def delete_supplementary_data_list_by_input_data_id(self, project_id: str, input_data_id_list: list[str]):  # noqa: ANN201
+    def delete_supplementary_data_list_by_input_data_id(self, project_id: str, input_data_id_list: list[str]) -> None:
         dict_deleted_count: dict[str, int] = {}
         for input_data_id in input_data_id_list:
             input_data = self.service.wrapper.get_input_data_or_none(project_id, input_data_id)
