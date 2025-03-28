f009b1eb5eec30f274e043ad12a77fa1c677e6b7
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2025-03-29 04:46:53 +0900

    lint

--- annofabcli/annotation/merge_segmentation.py
+++ annofabcli/annotation/merge_segmentation.py
@@ -329,7 +329,7 @@ class MergeSegmentation(CommandLine):
             label_id_list.append(label["label_id"])
 
         if len(invalid_label_name_list) > 0:
-            print(
+            print(  # noqa: T201
                 f"{self.COMMON_MESSAGE} --label_name: 次のラベル名(英語)はアノテーション仕様に存在しません。 :: {invalid_label_name_list}",
                 file=sys.stderr,
             )
