fd233ac587d7dc8eeddf52bff9675125a1542b67
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2025-04-25 15:19:38 +0900

    `--version`を指定したときに正しくバージョン情報が出力されるようにしました。 (#1381)
    * version情報が正しく出力されるようにする
    
    * uopdate versioning

--- annofabcli/__version__.py
+++ annofabcli/__version__.py
@@ -1,7 +1,1 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    # `uv run annofabcli --version`では、メタデータからバージョン情報を取得できないため、fallbackしたバージョンを設定する
-    __version__ = "0.0.0"
+__version__ = "1.100.4.post1.dev0+5a40ec9e"
