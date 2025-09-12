08e29f5a416d8f4db86548162661bc865a193355
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2025-09-12 16:42:45 +0900

    aadd

--- annofabcli/project/put_project.py
+++ annofabcli/project/put_project.py
@@ -10,8 +10,7 @@ import annofabcli.project.create_project
 
 def main(args: argparse.Namespace) -> None:
     print(  # noqa: T201
-        "⚠ 警告: `project put` コマンドは非推奨です。代わりに `project create` コマンドを使用してください。"
-        "`project put` コマンドは2026年01月01日以降に廃止予定です。",
+        "⚠ 警告: `project put` コマンドは非推奨です。代わりに `project create` コマンドを使用してください。`project put` コマンドは2026年01月01日以降に廃止予定です。",
         file=sys.stderr,
     )
     # create_project の main 関数を呼び出す
@@ -25,7 +24,7 @@ def parse_args(parser: argparse.ArgumentParser) -> None:
 
 def add_parser(subparsers: Optional[argparse._SubParsersAction] = None) -> argparse.ArgumentParser:
     subcommand_name = "put"
-    subcommand_help = "プロジェクトを作成します。"
+    subcommand_help = "[DEPRECATED] プロジェクトを作成します。"
     epilog = "組織管理者、組織オーナを持つユーザで実行してください。"
 
     parser = annofabcli.common.cli.add_parser(subparsers, subcommand_name, subcommand_help, epilog=epilog)
