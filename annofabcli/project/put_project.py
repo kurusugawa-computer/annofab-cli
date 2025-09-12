8d95d96451da18fe7572bf8784a41d4c011f596b
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2025-09-12 16:51:22 +0900

    updat

--- annofabcli/project/put_project.py
+++ annofabcli/project/put_project.py
@@ -5,26 +5,28 @@ import sys
 from typing import Optional
 
 import annofabcli
-import annofabcli.project.create_project
-
+from annofabcli.project import create_project
 
 def main(args: argparse.Namespace) -> None:
     print(  # noqa: T201
-        "⚠ 警告: `project put` コマンドは非推奨です。代わりに `project create` コマンドを使用してください。`project put` コマンドは2026年01月01日以降に廃止予定です。",
+        "⚠ 警告: `project put` コマンドは非推奨です。代わりに `project create` コマンドを使用してください。"
+        "`project put` コマンドは2026年01月01日以降に廃止予定です。",
         file=sys.stderr,
     )
-    # create_project の main 関数を呼び出す
-    annofabcli.project.create_project.main(args)
+    # create_project.py の実装を使用
+    create_project.main(args)
 
 
 def parse_args(parser: argparse.ArgumentParser) -> None:
-    # create_project の parse_args をそのまま利用
-    annofabcli.project.create_project.parse_args(parser)
+    # create_project.py のparse_argsと同じ実装を使用
+    create_project.parse_args(parser)
+    # main関数のみ差し替え
+    parser.set_defaults(subcommand_func=main)
 
 
 def add_parser(subparsers: Optional[argparse._SubParsersAction] = None) -> argparse.ArgumentParser:
     subcommand_name = "put"
-    subcommand_help = "[DEPRECATED] プロジェクトを作成します。"
+    subcommand_help = "プロジェクトを作成します。"
     epilog = "組織管理者、組織オーナを持つユーザで実行してください。"
 
     parser = annofabcli.common.cli.add_parser(subparsers, subcommand_name, subcommand_help, epilog=epilog)
