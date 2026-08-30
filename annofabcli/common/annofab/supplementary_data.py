68b840f74b80b88b3702cf932b8189f34511f0bc
yuji38kwmt <yuji38kwmt@yahoo.co.jp>
2026-08-30 15:47:50 +0900

    `supplementary_data.py`の`get_supplementary_data_dict_in_bulk`関数で、`supplementary_data_dict`の型注釈を追加

--- annofabcli/common/annofab/supplementary_data.py
+++ annofabcli/common/annofab/supplementary_data.py
@@ -9,7 +9,7 @@ from annofabcli.common.annofab.input_data import BULK_REQUEST_SIZE
 def get_supplementary_data_dict_in_bulk(service: annofabapi.Resource, project_id: str, input_data_id_list: Collection[str]) -> dict[str, list[dict[str, Any]]]:
     """補助情報をバルク取得し、入力データIDごとの辞書で返す。"""
     input_data_id_list = list(dict.fromkeys(input_data_id_list))
-    supplementary_data_dict = {input_data_id: [] for input_data_id in input_data_id_list}
+    supplementary_data_dict: dict[str, list[dict[str, Any]]] = {input_data_id: [] for input_data_id in input_data_id_list}
     for initial_index in range(0, len(input_data_id_list), BULK_REQUEST_SIZE):
         batch_input_data_id_list = input_data_id_list[initial_index : initial_index + BULK_REQUEST_SIZE]
         response, _ = service.api.get_supplementary_data_in_bulk(project_id, query_params={"input_data_id": ",".join(batch_input_data_id_list)})
